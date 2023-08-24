b28346a5a7c94d1f01a5c487911318a516cb89dd
w1shope <122812652+w1shope@users.noreply.github.com>
2023-08-24 16:59:09 +0900

    [level 2] Title: 카펫, Time: 0.02 ms, Memory: 74.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.java
+++ 프로그래머스/lv2/42842. 카펫/카펫.java
@@ -1,16 +1,12 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] result = new int[2];
-
         int width = (brown - 2) / 2;
         int height = 3;
-        while (true) {
-            if ((width - 2) * (height - 2) != yellow) {
-                width -= 1;
-                height += 1;
-            } else {
+        while(true) {
+            if((width - 2) * (height - 2) == yellow)
                 return new int[]{width, height};
-            }
+            width -= 1;
+            height += 1;
         }
     }
 }
