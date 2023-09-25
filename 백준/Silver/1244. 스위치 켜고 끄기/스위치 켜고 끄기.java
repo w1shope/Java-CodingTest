a06fb126129683fb22a0d31ac7640358a14b1f43
이희망 <122812652+w1shope@users.noreply.github.com>
2023-09-26 08:45:59 +0900

    [Silver IV] Title: 스위치 켜고 끄기, Time: 148 ms, Memory: 15860 KB -BaekjoonHub

--- 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
+++ 백준/Silver/1244. 스위치 켜고 끄기/스위치 켜고 끄기.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -28,7 +29,7 @@ public class Main {
 
         int count = 0;
         for (int i = 0; i < n; i++) {
-            if (count == 20) {
+            if(count == 20) {
                 sb.append("\n");
                 count = 0;
             }
@@ -39,7 +40,6 @@ public class Main {
         System.out.println(sb);
     }
 
-    // 남자일 경우
     private static void stateChangeByMan(int[] switchs, int switchNum) {
         for (int i = switchNum; i < switchs.length; i += switchNum + 1) {
             if (switchs[i] == 1)
@@ -49,7 +49,6 @@ public class Main {
         }
     }
 
-    // 여자일 경우
     private static void stateChangeByWomen(int[] switchs, int switchNum) {
         int count = 1;
         while (true) {
@@ -60,19 +59,21 @@ public class Main {
                 break;
 
             if (switchs[leftSwitchNum] == switchs[rightSwitchNum]) {
-                stateChange(switchs, leftSwitchNum);
-                stateChange(switchs, rightSwitchNum);
+                if (switchs[leftSwitchNum] == 1)
+                    switchs[leftSwitchNum] = 0;
+                else
+                    switchs[leftSwitchNum] = 1;
+
+                if (switchs[rightSwitchNum] == 1)
+                    switchs[rightSwitchNum] = 0;
+                else
+                    switchs[rightSwitchNum] = 1;
             } else
                 break;
 
             count += 1;
         }
 
-        stateChange(switchs, switchNum);
-    }
-
-    // 전구 상태 변경 0 <-> 1
-    private static void stateChange(int[] switchs, int switchNum) {
         if (switchs[switchNum] == 1)
             switchs[switchNum] = 0;
         else
