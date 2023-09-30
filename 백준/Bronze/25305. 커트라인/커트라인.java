631a76cd9899555cbdec5871e89c0dd7b28c8b9b
이희망 <122812652+w1shope@users.noreply.github.com>
2023-09-30 19:17:39 +0900

    [Bronze II] Title: 커트라인, Time: 136 ms, Memory: 14604 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,18 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] input = br.readLine().split(" ");
+        int n = Integer.parseInt(input[0]);
+        int k = Integer.parseInt(input[1]);
+        int[] arr = new int[n];
+        input = br.readLine().split(" ");
+        for (int i = 0; i < n; i++)
+            arr[i] = Integer.parseInt(input[i]);
+        System.out.println(Arrays.stream(arr).sorted().skip(n - k).min().getAsInt());
+    }
+}
