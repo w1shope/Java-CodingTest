888fbbe0459dd469d1fbbe898a534271a356a7b1
희망 <122812652+w1shope@users.noreply.github.com>
2023-11-10 17:16:15 +0900

    [Silver IV] Title: 카드, Time: 404 ms, Memory: 32276 KB -BaekjoonHub

--- 백준/Silver/11652. 카드/카드.java
+++ 백준/Silver/11652. 카드/카드.java
@@ -1,24 +1,28 @@
-import java.io.*;
-import java.math.BigInteger;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException{
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int n = Integer.parseInt(br.readLine());
-        HashMap<BigInteger, Integer> map = new HashMap<>(n);
-        for (int i=0; i<n; i++){
-            BigInteger b = new BigInteger(br.readLine());
-            if (map.containsKey(b))
-                map.put(b, map.get(b)+1);
-            else map.put(b, 1);
+        Map<Long, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < n; i++) {
+            Long s = Long.parseLong(br.readLine());
+            if (map.containsKey(s))
+                map.put(s, map.get(s) + 1);
+            else
+                map.put(s, 1);
         }
-        System.out.print(map.entrySet()
-                            .stream()
-                            .sorted((entry1, entry2) -> entry1.getKey().compareTo(entry2.getKey()))
-                            .max((entry1, entry2) -> entry1.getValue().compareTo(entry2.getValue()))
-                            .get()
-                            .getKey()
+        System.out.println(
+                map.entrySet().stream()
+                        .sorted((e1, e2) -> e1.getKey().compareTo(e2.getKey()))
+                        .max((e1, e2) -> e1.getValue().compareTo(e2.getValue()))
+                        .get()
+                        .getKey()
         );
     }
 }
