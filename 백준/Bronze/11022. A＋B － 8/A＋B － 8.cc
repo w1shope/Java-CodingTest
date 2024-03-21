9d13fc7ebc28b725da78aae8cdaad68f046f2ce6
희망 <122812652+w1shope@users.noreply.github.com>
2024-03-22 01:59:20 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+
+    for(int i = 0; i < n; i++) {
+        int a, b;
+        scanf("%d %d", &a, &b);
+        printf("Case #%d: %d + %d = %d\n", (i+1), a, b, a+ b);
+    }
+}
