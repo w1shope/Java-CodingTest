5b8d3ce8278c69834120e64870656ca9ebe02c0e
희망 <122812652+w1shope@users.noreply.github.com>
2024-03-26 18:31:29 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 272 ms, Memory: 5804 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n;
+    cin >> n;
+
+    int arr[n];
+    for(int i = 0; i < n; i++) {
+        cin >> arr[i];
+    }
+
+    sort(arr, arr + n);
+
+    for(int i = 0; i < n; i++) {
+        cout << arr[i] << '\n';
+    }
+}
