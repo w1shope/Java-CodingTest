b49f09c694f373584e3621c8406724ae97e7164e
희망 <122812652+w1shope@users.noreply.github.com>
2024-04-07 17:44:35 +0900

    [Silver V] Title: 생일, Time: 0 ms, Memory: 2032 KB -BaekjoonHub

--- 백준/Silver/5635. 생일/생일.cc
+++ 백준/Silver/5635. 생일/생일.cc
@@ -1,47 +1,9 @@
 #include <iostream>
-#include <sstream>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
 
-vector<int> split(string s) {
-    stringstream ss(s);
-    string buffer;
-    vector<int> v;
-    getline(ss, buffer, ' ');
-    while (getline(ss, buffer, ' ')) {
-        v.push_back(stoi(buffer));
-    }
-    return v;
-}
-
-string getName(string s) {
-    stringstream ss(s);
-    string buffer;
-    getline(ss, buffer, ' ');
-    return buffer;
-}
-
-bool compare(string s1, string s2) {
-    vector<int> v1 = split(s1);
-    vector<int> v2 = split(s2);
-
-    int year1 = v1[2];
-    int year2 = v2[2];
-    int month1 = v1[1];
-    int month2 = v2[1];
-    int day1 = v1[0];
-    int day2 = v2[0];
-
-    if (year1 != year2)
-        return year1 < year2;
-
-    if (month1 != month2)
-        return month1 < month2;
-
-    return day1 < day2;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -51,11 +13,13 @@ int main() {
     int n;
     (cin >> n).ignore();
 
-    vector<string> sArr(n);
+    vector<pair<pair<int, int>, pair<int, string> > > v(n);
+
     for (int i = 0; i < n; i++)
-        getline(cin, sArr[i]);
+        cin >> v[i].second.second >> v[i].second.first
+            >> v[i].first.second >> v[i].first.first;
 
-    sort(sArr.begin(), sArr.end(), compare);
+    sort(v.begin(), v.end());
 
-    cout << getName(sArr[n - 1]) << "\n" << getName(sArr[0]);
+    cout << v[n - 1].second.second << "\n" << v[0].second.second;
 }
