3f66bf5187bb6ead089c91b7c94d0c3d90e87c6b
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-19 09:36:08 +0900

    [Silver V] Title: 숫자 빈도수, Time: 176 ms, Memory: 17680 KB -BaekjoonHub

--- 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
+++ 백준/Silver/14912. 숫자 빈도수/숫자 빈도수.java
@@ -9,12 +9,15 @@ public class Main {
         int result = 0;
         int n = Integer.parseInt(split[0]);
         int find = Integer.parseInt(split[1]);
+
         for (int i = 1; i <= n; i++) {
-            String strNumber = String.valueOf(i);
-            for (char c : strNumber.toCharArray()) {
-                if (c - '0' == find) {
+            int number = i;
+            while (number != 0) {
+                int div = number % 10;
+                if (div == find) {
                     result++;
                 }
+                number /= 10;
             }
         }
 
