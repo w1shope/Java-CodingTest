1406892b40e812f12508b30b5cdc2d0cd3c2c5c8
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-19 09:07:05 +0900

    [Silver V] Title: 숫자 게임, Time: 140 ms, Memory: 16540 KB -BaekjoonHub

---
+++ 백준/Silver/2303. 숫자 게임/숫자 게임.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int tc = Integer.parseInt(br.readLine());
+        int[] result = new int[tc];
+        for (int t = 0; t < tc; t++) {
+            String[] split = br.readLine().split(" ");
+            int[] numbers = Arrays.stream(split)
+                    .mapToInt(Integer::valueOf)
+                    .toArray();
+            int bigLastNumber = 0;
+            for (int i = 0; i < numbers.length - 2; i++) {
+                for (int j = i + 1; j < numbers.length - 1; j++) {
+                    for (int k = j + 1; k < numbers.length; k++) {
+                        int number = numbers[i] + numbers[j] + numbers[k];
+                        String strNumber = String.valueOf(number);
+                        int lastNumber = strNumber.charAt(strNumber.length() - 1) - '0';
+                        if (lastNumber > bigLastNumber) {
+                            bigLastNumber = lastNumber;
+                        }
+                    }
+                }
+            }
+            result[t] = bigLastNumber;
+        }
+
+        int personNumber = 1;
+        int bigNumber = result[0];
+        for (int i = 1; i < result.length; i++) {
+            if (result[i] >= bigNumber) {
+                personNumber = i + 1;
+                bigNumber = result[i];
+            }
+        }
+
+        System.out.println(personNumber);
+    }
+}
