c942ac0af865532b23c6c46308fb2316e43a9bbd
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-23 21:23:49 +0900

    [Silver IV] Title: 한수, Time: 104 ms, Memory: 14252 KB -BaekjoonHub

--- 백준/Silver/1065. 한수/한수.java
+++ 백준/Silver/1065. 한수/한수.java
@@ -10,8 +10,12 @@ public class Main {
 
         int result = 0;
         for (int num = 1; num <= N; num++) {
-            if (isTrue(num)) {
+            if (num < 100) {
                 result++;
+            } else {
+                if (isTrue(num)) {
+                    result++;
+                }
             }
         }
 
@@ -20,22 +24,13 @@ public class Main {
 
     // 한수이면 true
     static boolean isTrue(int number) {
-        if (number < 100) { // 3자리 수 이전의 값들은 모두 한 수이다.
+        int first = number / 100;
+        int second = number % 100 / 10;
+        int third = number % 10 / 1;
+
+        if ((first - second) == (second - third)) {
             return true;
         }
-        String strNumber = String.valueOf(number);
-
-        int zeroIdxNumber = strNumber.charAt(0) - '0';
-        int prev = strNumber.charAt(1) - '0';
-        int diff = zeroIdxNumber - prev;
-        
-        for (int i = 2; i < strNumber.length(); i++) {
-            int now = strNumber.charAt(i) - '0';
-            if (prev - now != diff) {
-                return false;
-            }
-            prev = now;
-        }
-        return true;
+        return false;
     }
 }
