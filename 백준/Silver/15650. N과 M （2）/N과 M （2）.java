b60b23302ab1c2795ee06958cb10030fc159a63b
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-24 13:29:46 +0900

    [Silver III] Title: N과 M (2), Time: 120 ms, Memory: 15760 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int N, M;
+    static int[] printNumbers;
+    static StringBuilder sb = new StringBuilder();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        String[] inputArr = br.readLine().split(" ");
+        N = Integer.parseInt(inputArr[0]);
+        M = Integer.parseInt(inputArr[1]);
+
+        printNumbers = new int[M];
+
+        recur(1, 0);
+
+        System.out.println(sb.toString());
+    }
+
+    static void recur(int start, int depth) {
+        if (depth == M) {
+            for (int i = 0; i < M; i++) {
+                sb.append(printNumbers[i] + " ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i = start; i <= N; i++) {
+            printNumbers[depth] = i;
+            recur(i + 1, depth + 1);
+        }
+
+    }
+}
