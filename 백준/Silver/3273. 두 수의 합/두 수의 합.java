ddee8dab70a77c77299f68f416eba4149a4d2b70
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-27 09:30:36 +0900

    [Silver III] Title: 두 수의 합, Time: 380 ms, Memory: 33016 KB -BaekjoonHub

--- 백준/Silver/3273. 두 수의 합/두 수의 합.java
+++ 백준/Silver/3273. 두 수의 합/두 수의 합.java
@@ -1,36 +1,44 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
 
 public class Main {
+
+    static int N;
+    static int[] arr;
+    static int X;
+    static int answer;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        X = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        Set<Integer> set = new HashSet<>();
-        String[] input = br.readLine().split(" ");
-        for (int i = 0; i < n; i++)
-            set.add(Integer.parseInt(input[i]));
-        int x = Integer.parseInt(br.readLine());
+        Arrays.sort(arr); // 오름차순 정렬
 
-        int[] arr = set.stream().mapToInt(Integer::intValue).toArray();
-        Arrays.sort(arr);
+        int s = 0, e = N - 1; // 투 포인터
+        traverse(s, e);
 
-        int count = 0;
-        for (int i = 0; i < n; i++) {
-            if (arr[i] >= x)
-                break;
-            for (int j = i + 1; j < n; j++) {
-                int sum = arr[i] + arr[j];
-                if (sum == x)
-                    count++;
-                else if (sum > x) {
-                    break;
-                }
+        System.out.println(answer);
+    }
+
+    static void traverse(int s, int e) {
+        while (s < e) {
+            int sum = arr[s] + arr[e];
+            if (sum == X) {
+                answer++;
+                e--;
+            }
+            if (sum > X) {
+                e--;
+            }
+            if (sum < X) {
+                s++;
             }
         }
-
-        System.out.println(count);
     }
 }
