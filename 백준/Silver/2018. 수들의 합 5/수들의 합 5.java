fdc94d5cb3979938101f6c71373306f7d86df2d1
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-27 10:43:22 +0900

    [Silver V] Title: 수들의 합 5, Time: 224 ms, Memory: 17792 KB -BaekjoonHub

--- 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
+++ 백준/Silver/2018. 수들의 합 5/수들의 합 5.java
@@ -5,32 +5,15 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         int N = sc.nextInt();
 
-        if(N == 1) {
-            System.out.println(1);
-            return;
-        }
-
-        int[] arr = new int[N];
-        for (int i = 0; i < N; i++) {
-            arr[i] = i + 1;
-        }
-
-        int s = 0, e = 1; // 투 포인터
-        int answer = 0;
-        int sum = arr[s] + arr[e];
-        while (s <= e && e < N) {
-            if (sum == N) { // 정답
+        int s = 1, e = 1, sum = 1, answer = 1;
+        while (e < N) {
+            if (sum == N) { // 정답 조건
                 answer++;
-                sum -= arr[s++];
-                if (e < N - 1) {
-                    sum += arr[++e];
-                }
-            } else if (sum > N) { // 연속된 배열의 합이 더 크다면
-                sum -= arr[s++];
+                sum += ++e;
+            } else if (sum < N) {
+                sum += ++e;
             } else {
-                if (e < N - 1) {
-                    sum += arr[++e];
-                }
+                sum -= s++;
             }
         }
 
