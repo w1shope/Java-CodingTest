74f2c26d00a0e9f17051cb7830d6b080f5d5cd04
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-28 19:39:21 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 3, Time: 1048 ms, Memory: 150708 KB -BaekjoonHub

---
+++ 백준/Silver/24446. 알고리즘 수업 － 너비 우선 탐색 3/알고리즘 수업 － 너비 우선 탐색 3.java
@@ -0,0 +1,79 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+public class Main {
+
+    static int N, M, R;
+    static int[] answer;
+    static List<List<Integer>> nodes = new ArrayList<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int[] inputArr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        N = inputArr[0];
+        M = inputArr[1];
+        R = inputArr[2];
+
+        for (int i = 0; i <= N; i++) {
+            nodes.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            inputArr = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::valueOf)
+                    .toArray();
+            int v1 = inputArr[0];
+            int v2 = inputArr[1];
+            nodes.get(v1).add(v2);
+            nodes.get(v2).add(v1);
+        }
+
+        answer = new int[N + 1];
+        Arrays.fill(answer, -1);
+
+        boolean[] visited = new boolean[N + 1];
+        bfs(R, visited, 0);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(answer[i] + "\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void bfs(int node, boolean[] visited, int visitOrder) {
+        visited[node] = true;
+        answer[node] = visitOrder++;
+
+        Queue<Integer> que = new LinkedList<>();
+        que.offer(node);
+
+        Queue<Integer> tmp = new LinkedList<>();
+        while (!que.isEmpty()) {
+            while (!que.isEmpty()) {
+                int poll = que.poll();
+                for (int v : nodes.get(poll)) {
+                    if (!visited[v]) {
+                        visited[v] = true;
+                        tmp.offer(v);
+                        answer[v] = visitOrder;
+                    }
+                }
+            }
+            visitOrder++;
+            while (!tmp.isEmpty()) {
+                que.offer(tmp.poll());
+            }
+        }
+    }
+}
