f52d561d3b86e440eaeec5879b773c1d3e81fdcd
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-10-31 18:15:28 +0900

    [Silver IV] Title: 우당탕탕 영화예매, Time: 344 ms, Memory: 28284 KB -BaekjoonHub

---
+++ 백준/Silver/29700. 우당탕탕 영화예매/우당탕탕 영화예매.java
@@ -0,0 +1,42 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+
+    static int N, M, K;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputArr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        N = inputArr[0];
+        M = inputArr[1];
+        K = inputArr[2];
+
+        int answer = 0;
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine();
+            int zeroCount = 0;
+            for (int j = 0; j < M; j++) {
+                if (str.charAt(j) == '1') {
+                    if (zeroCount >= K) {
+                        answer += 1 + (zeroCount - K);
+                    }
+                    zeroCount = 0;
+                } else if (str.charAt(j) == '0') {
+                    zeroCount++;
+                    if (j == M - 1) {
+                        if (zeroCount >= K) {
+                            answer += 1 + (zeroCount - K);
+                        }
+                    }
+                }
+            }
+        }
+
+        System.out.println(answer);
+    }
+}
