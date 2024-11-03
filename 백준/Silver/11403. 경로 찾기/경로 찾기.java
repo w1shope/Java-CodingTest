7fe28a40b15f102462e79d1052db61479d9d9f4f
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-04 01:02:04 +0900

    [Silver I] Title: 경로 찾기, Time: 188 ms, Memory: 17652 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.java
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.java
@@ -1,41 +1,49 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Main {
 
+    static int N;
+    static int[][] arr;
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
+
+        for (int i = 0; i < N; i++) {
+            arr[i] = Arrays.stream(br.readLine().split(" "))
+                    .mapToInt(Integer::valueOf)
+                    .toArray();
+        }
+
+        floydWarshall();
+
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        int[][] graph = new int[n][n];
-        for (int i = 0; i < n; i++) {
-            String[] inputs = br.readLine().split(" ");
-            for (int j = 0; j < n; j++) {
-                graph[i][j] = Integer.parseInt(inputs[j]);
-            }
-        }
-
-        for (int k = 0; k < n; k++) {
-            for (int y = 0; y < n; y++) {
-                for (int x = 0; x < n; x++) {
-                    if (graph[y][k] == 1 && graph[k][x] == 1) {
-                        graph[y][x] = 1;
-                    }
-                }
-            }
-        }
-
-        for (int y = 0; y < n; y++) {
-            for (int x = 0; x < n; x++) {
-                sb.append(graph[y][x] + " ");
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                sb.append(arr[i][j] + " ");
             }
             sb.append("\n");
         }
 
         System.out.println(sb);
 
+    }
+
+    static void floydWarshall() {
+        for (int k = 0; k < N; k++) {
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    if (arr[i][j] != 1) {
+                        if (arr[i][k] == 1 && arr[k][j] == 1) {
+                            arr[i][j] = 1;
+                        }
+                    }
+                }
+            }
+        }
     }
 }
