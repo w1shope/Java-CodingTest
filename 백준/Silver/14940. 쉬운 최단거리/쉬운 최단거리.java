67a2a8e112348109d24843f2b621eb5f628efd93
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-04 10:06:04 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 696 ms, Memory: 69484 KB -BaekjoonHub

---
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class Main {
+
+    static int N, M;
+    static int[][] arr;
+    static int[][] answer;
+    static int[] dx = {0, 0, -1, 1};
+    static int[] dy = {-1, 1, 0, 0};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int[] inputArr = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        N = inputArr[0];
+        M = inputArr[1];
+
+        arr = new int[N][M];
+        int targetX = -1;
+        int targetY = -1;
+        for (int i = 0; i < N; i++) {
+            String[] str = br.readLine().split(" ");
+            for (int j = 0; j < M; j++) {
+                int num = Integer.parseInt(str[j]);
+                if (num == 2) {
+                    targetX = j;
+                    targetY = i;
+                }
+                arr[i][j] = num;
+            }
+        }
+
+        answer = new int[N][M];
+        answer[targetY][targetX] = 0;
+
+        bfs(targetX, targetY);
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                if (arr[i][j] == 1) {
+                    sb.append(-1 + " ");
+                } else {
+                    sb.append(answer[i][j] + " ");
+                }
+            }
+            sb.append("\n");
+        }
+
+        System.out.println(sb);
+    }
+
+    static void bfs(int startX, int startY) {
+        int count = 0;
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{startX, startY, count});
+        arr[startY][startX] = 0;
+
+        while (!que.isEmpty()) {
+            int[] pos = que.poll();
+            for (int d = 0; d < 4; d++) {
+                int cx = pos[0] + dx[d];
+                int cy = pos[1] + dy[d];
+                if (isNotOutOfRange(cx, cy) && arr[cy][cx] == 1) {
+                    answer[cy][cx] = pos[2] + 1;
+                    que.offer(new int[]{cx, cy, pos[2] + 1});
+                    arr[cy][cx] = 0;
+                }
+            }
+        }
+    }
+
+    static boolean isNotOutOfRange(int x, int y) {
+        return x >= 0 && y >= 0 && x < M && y < N;
+    }
+}
