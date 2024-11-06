f2eea3fcb6b649d2c985df9cb0d5c46f2ea298f7
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-06 21:13:14 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 864 ms, Memory: 103680 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -3,61 +3,49 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.LinkedList;
 import java.util.List;
-import java.util.Queue;
 
 public class Main {
 
     static int N;
-    static List<List<Integer>> nodes = new ArrayList<>();
+    static List<List<Integer>> graph;
+    static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         N = Integer.parseInt(br.readLine());
-        for (int i = 0; i <= N; i++) {
-            nodes.add(new ArrayList<>());
-        }
 
+        graph = new ArrayList<>();
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
         for (int i = 0; i < N - 1; i++) {
             int[] inputArr = Arrays.stream(br.readLine().split(" "))
                     .mapToInt(Integer::valueOf)
                     .toArray();
             int v1 = inputArr[0];
             int v2 = inputArr[1];
-            nodes.get(v1).add(v2);
-            nodes.get(v2).add(v1);
+            graph.get(v1).add(v2); 
+            graph.get(v2).add(v1); // 무방향 그래프
         }
 
-        int[] arr = new int[N + 1];
-        bfs(1, new boolean[N + 1], arr);
+        answer = new int[N + 1];
+        dfs(1);
 
+        StringBuilder sb = new StringBuilder();
         for (int i = 2; i <= N; i++) {
-            sb.append(arr[i] + "\n");
+            sb.append(answer[i] + "\n");
         }
-
         System.out.println(sb);
     }
 
-    static void bfs(int v, boolean[] visited, int[] arr) {
-        visited[v] = true; // 현재 노드 방문 처리
-        Queue<Integer> que = new LinkedList<>();
-        que.offer(v);
-
-        while (!que.isEmpty()) {
-            int poll = que.poll();
-            for (int node : nodes.get(poll)) {
-                if (visited[node]) {
-                    continue;
-                }
-                visited[node] = true;
-                que.offer(node);
-                arr[node] = poll;
+    static void dfs(int node) {
+        for (int v : graph.get(node)) { // 연결된 모든 노드 탐색
+            if (answer[v] == 0) { // 부모가 결정되지 않았다면
+                answer[v] = node; // v는 node로부터 파생된 노드
+                dfs(v);
             }
         }
     }
-
 
 }
