9a47fabad4287d3a511d4ba7093b54568ad42fd8
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-07 10:05:50 +0900

    [Silver I] Title: 봄버맨, Time: 236 ms, Memory: 15452 KB -BaekjoonHub

--- 백준/Silver/16918. 봄버맨/봄버맨.java
+++ 백준/Silver/16918. 봄버맨/봄버맨.java
@@ -1,77 +1,101 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Arrays;
 
 public class Main {
-    private static final int[] dx = {1, -1, 0, 0};
-    private static final int[] dy = {0, 0, 1, -1};
-    private static char[][] board;
-    private static List<int[]> bombCoordinate = new ArrayList<>();
+
+    static int R, C, N;
+    static char[][] bombs;
+    static boolean[][] visited;
+    static int[] dx = {0, 0, 0, -1, 1};
+    static int[] dy = {0, -1, 1, 0, 0};
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        R = inputs[0]; // 가로
+        C = inputs[1]; // 세로
+        N = inputs[2]; // 몇 초?
 
-        String[] input = br.readLine().split(" ");
-        int r = Integer.parseInt(input[0]);
-        int c = Integer.parseInt(input[1]);
-        int n = Integer.parseInt(input[2]);
-
-        board = new char[r][c];
-        for (int i = 0; i < r; i++) {
-            String str = br.readLine();
-            for (int j = 0; j < c; j++)
-                board[i][j] = str.charAt(j);
+        visited = new boolean[R][C];
+        bombs = new char[R][C];
+        for (int i = 0; i < R; i++) {
+            char[] c = br.readLine().toCharArray();
+            for (int j = 0; j < C; j++) {
+                bombs[i][j] = c[j];
+                if (c[j] == 'O') {
+                    // 모든 폭탄이 설치되기 전에, 폭탄이 설치된 좌표
+                    // 즉, 터뜨릴 폭탄 좌표
+                    visited[i][j] = true;
+                }
+            }
         }
 
-        int time = 0;
-        for (int i = 0; i < n - 1; i++) {
-            if (time == 0) {
-                fillBoard();
-                time = 1;
+        execute();
+
+        StringBuilder result = new StringBuilder();
+        for (int y = 0; y < R; y++) {
+            for (char c : bombs[y]) {
+                result.append(c);
+            }
+            result.append("\n");
+        }
+        System.out.println(result);
+    }
+
+    static void execute() {
+        // 1초 : 폭탄이 설치되지 않은 모든 곳에 폭탄 설치
+        // 2초 : 폭탄 터뜨린다. -> (1)visited=true, (2)인접한 폭탄, (3)터지지 않은 폭탄의 좌표를 visited에 저장
+        // 1 ~ 2 반복
+        for (int count = 1; count < N; count++) {
+            if (count % 2 == 0) {
+                strikeBomb(); // 폭탄을 터뜨린다.
+                storeBombPos(); // 폭탄이 설치된 좌표를 저장한다.
             } else {
-                explodeBomb();
-                time = 0;
-            }
-        }
-
-        for (int i = 0; i < r; i++) {
-            for (int j = 0; j < c; j++)
-                sb.append(board[i][j]);
-            sb.append("\n");
-        }
-
-        System.out.println(sb);
-    }
-
-    /**
-     * 숫자 0이 아니라 알파벳 대문자 'O'입니다.
-    */
-    private static void fillBoard() {
-        for (int i = 0; i < board.length; i++) {
-            for (int j = 0; j < board[i].length; j++) {
-                if (board[i][j] == 'O')
-                    bombCoordinate.add(new int[]{j, i});
-                board[i][j] = 'O';
+                // 폭탄이 설치되지 않은 곳에 폭탄을 설치한다.
+                putBomb();
             }
         }
     }
 
-    private static void explodeBomb() {
-        for (int[] coordinate : bombCoordinate) {
-            int x = coordinate[0];
-            int y = coordinate[1];
-            board[y][x] = '.';
-            for (int d = 0; d < 4; d++) {
-                int nx = x + dx[d];
-                int ny = y + dy[d];
-                if (ny < 0 || ny >= board.length || nx < 0 || nx >= board[ny].length)
-                    continue;
-                board[ny][nx] = '.';
+    static void putBomb() {
+        for (int y = 0; y < R; y++) {
+            Arrays.fill(bombs[y], 'O');
+        }
+    }
+
+    static void storeBombPos() {
+        for (int y = 0; y < R; y++) {
+            for (int x = 0; x < C; x++) {
+                if (bombs[y][x] == 'O') {
+                    visited[y][x] = true;
+                }
             }
         }
-        bombCoordinate.clear();
     }
+
+    static void strikeBomb() {
+        for (int y = 0; y < R; y++) {
+            for (int x = 0; x < C; x++) {
+                if (visited[y][x]) { // 폭탄이 설치되어 있다면
+                    visited[y][x] = false;
+                    for (int d = 0; d < 5; d++) { // 자신을 포함한 인접한 좌표의 폭탄을 같이 터뜨린다.
+                        int cx = x + dx[d];
+                        int cy = y + dy[d];
+                        if (isNotOutOfRange(cx, cy)) {
+                            bombs[cy][cx] = '.'; // 평지가 된다.
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    static boolean isNotOutOfRange(int x, int y) {
+        return x >= 0 && y >= 0 && x < C && y < R;
+    }
+
 }
