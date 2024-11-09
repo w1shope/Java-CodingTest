5a5e4d8c646bfea50a54d81cde353d3e81f8da1b
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-09 22:07:22 +0900

    [Silver IV] Title: ATM, Time: 124 ms, Memory: 14648 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.java
+++ 백준/Silver/11399. ATM/ATM.java
@@ -1,30 +1,55 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.stream.IntStream;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
+
+    static int N;
+    static List<Person> persons = new ArrayList<>();
+
     public static void main(String[] args) throws IOException {
-
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
 
-        int n = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-
-        int[] nums = new int[n];
-        for(int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(input[i]);
+        for (int i = 1; i <= N; i++) {
+            persons.add(new Person(i, inputs[i - 1]));
         }
 
-        Arrays.sort(nums);
+        Collections.sort(persons);
 
-        int sum = 0;
-        for(int i = 0; i < n; i++) {
-            sum += nums[i];
-            nums[i] = sum;
+        System.out.println(answer());
+    }
+
+    static int answer() {
+        int totalTime = persons.get(0).time;
+        int personTime = totalTime;
+        for (int i = 1; i < N; i++) {
+            Person person = persons.get(i);
+            personTime += person.time;
+            totalTime += personTime;
         }
-        System.out.println(IntStream.of(nums).sum());
+        return totalTime;
+    }
 
+    static class Person implements Comparable<Person> {
+        int idx;
+        int time;
+
+        public Person(int idx, int time) {
+            this.idx = idx;
+            this.time = time;
+        }
+
+        @Override
+        public int compareTo(Person other) {
+            return Integer.compare(this.time, other.time);
+        }
     }
 }
