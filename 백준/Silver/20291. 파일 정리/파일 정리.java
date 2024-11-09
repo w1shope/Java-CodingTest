0caf915cc2ae5a7caaa35dbd5a2a9fa6c0e5b49f
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-09 20:59:16 +0900

    [Silver III] Title: 파일 정리, Time: 916 ms, Memory: 74160 KB -BaekjoonHub

--- 백준/Silver/20291. 파일 정리/파일 정리.java
+++ 백준/Silver/20291. 파일 정리/파일 정리.java
@@ -1,24 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
+
+    static int N;
+    static Map<String, Extension> map = new HashMap<>();
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
+        N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-
-        Map<String, Integer> map = new HashMap<>();
-        for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            String extension = input.substring(input.indexOf(".") + 1);
-
-            map.put(extension, map.getOrDefault(extension, 0) + 1);
+        for (int i = 0; i < N; i++) {
+            String[] splits = br.readLine().split("\\.");
+            String extension = splits[1];
+            if (map.containsKey(extension)) {
+                map.get(extension).count += 1;
+            } else {
+                map.put(extension, new Extension(extension, 1));
+            }
         }
 
-        map.keySet().stream().sorted().forEach(extension -> sb.append(extension + " " + map.get(extension) + "\n"));
-        System.out.println(sb);
+        List<Extension> extensions = new ArrayList<>(map.values());
+        Collections.sort(extensions);
+
+        StringBuilder answer = new StringBuilder();
+        for (Extension extension : extensions) {
+            answer.append(extension.name + " " + extension.count).append("\n");
+        }
+
+        System.out.println(answer);
+    }
+
+    static class Extension implements Comparable<Extension> {
+        String name;
+        int count;
+
+        public Extension(String name, int count) {
+            this.name = name;
+            this.count = count;
+        }
+
+        @Override
+        public int compareTo(Extension other) {
+            return this.name.compareTo(other.name);
+        }
     }
 }
