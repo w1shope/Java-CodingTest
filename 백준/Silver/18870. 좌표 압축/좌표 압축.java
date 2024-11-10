193d1de036854a4fafbd2682836a8bd8b9301054
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-10 15:17:19 +0900

    [Silver II] Title: 좌표 압축, Time: 2024 ms, Memory: 367212 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,52 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 public class Main {
 
     static int N;
-    static Map<Integer, List<Integer>> map = new HashMap<>();
-    static int[] answer;
+    static int[] originalArr;
+    static int[] sortedArr;
+    static Map<Integer, Integer> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+        originalArr = new int[N];
+        sortedArr = new int[N];
+
         int[] inputs = Arrays.stream(br.readLine().split(" "))
                 .mapToInt(Integer::valueOf)
                 .toArray();
         for (int i = 0; i < N; i++) {
-            if (map.containsKey(inputs[i])) {
-                List<Integer> values = map.get(inputs[i]);
-                values.add(i);
-            } else {
-                List<Integer> values = new ArrayList<>();
-                values.add(i);
-                map.put(inputs[i], values);
-            }
+            originalArr[i] = inputs[i];
+            sortedArr[i] = originalArr[i];
         }
 
-        answer = new int[N];
-        Set<Integer> set = map.keySet();
-        List<Integer> sortedList = new ArrayList<>(set);
-        Collections.sort(sortedList);
-        for (int i = 0; i < sortedList.size(); i++) {
-            int num = sortedList.get(i);
-            List<Integer> idxs = map.get(num);
-            for (int idx : idxs) {
-                answer[idx] = i;
+        Arrays.sort(sortedArr);
+
+        int order = 0;
+        for (int num : sortedArr) {
+            if (!map.containsKey(num)) {
+                map.put(num, order++);
             }
         }
 
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < N; i++) {
-            sb.append(answer[i] + " ");
+        for (int num : originalArr) {
+            sb.append(map.get(num) + " ");
         }
         System.out.println(sb);
 
