ad96687d71d00ed91fe2ffe5ac9d16f66eeca9e2
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-10 15:07:41 +0900

    [Silver II] Title: 좌표 압축, Time: 2628 ms, Memory: 404404 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,31 +1,54 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 public class Main {
-    private static Map<Integer, Integer> map = new HashMap<>();
+
+    static int N;
+    static Map<Integer, List<Integer>> map = new HashMap<>();
+    static int[] answer;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        for (int i = 0; i < N; i++) {
+            if (map.containsKey(inputs[i])) {
+                List<Integer> values = map.get(inputs[i]);
+                values.add(i);
+            } else {
+                List<Integer> values = new ArrayList<>();
+                values.add(i);
+                map.put(inputs[i], values);
+            }
+        }
+
+        answer = new int[N];
+        Set<Integer> set = map.keySet();
+        List<Integer> sortedList = new ArrayList<>(set);
+        Collections.sort(sortedList);
+        for (int i = 0; i < sortedList.size(); i++) {
+            int num = sortedList.get(i);
+            List<Integer> idxs = map.get(num);
+            for (int idx : idxs) {
+                answer[idx] = i;
+            }
+        }
+
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-        String[] input = br.readLine().split(" ");
-        Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < n; i++)
-            set.add(Integer.parseInt(input[i]));
-
-        List<Integer> sortedList = set.stream().sorted().collect(Collectors.toList());
-
-        int count = 0;
-        for (int num : sortedList)
-            map.put(num, count++);
-
-        for (int i = 0; i < n; i++)
-            sb.append(map.get(Integer.parseInt(input[i])) + " ");
-
+        for (int i = 0; i < N; i++) {
+            sb.append(answer[i] + " ");
+        }
         System.out.println(sb);
+
     }
 }
