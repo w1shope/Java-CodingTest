f68b1393941343bef28170c6454fdc2a4c8910af
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-10 13:24:04 +0900

    [Silver III] Title: 2×n 타일링, Time: 172 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.java
@@ -0,0 +1,22 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Main {
+
+    static int N;
+    static final int DIV = 10_007;
+    static int[] dp = new int[1001];
+
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        N = sc.nextInt();
+        dp[1] = 1;
+        dp[2] = 2;
+        for (int i = 3; i <= N; i++) {
+            dp[i] = (dp[i - 2] + dp[i - 1]) % DIV;
+        }
+
+        System.out.println(dp[N]);
+    }
+
+}
