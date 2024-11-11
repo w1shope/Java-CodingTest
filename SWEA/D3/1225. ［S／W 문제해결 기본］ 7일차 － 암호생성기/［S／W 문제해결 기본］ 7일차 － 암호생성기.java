127306b45983dae8eefc76e02395b3464870b196
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-11 23:14:52 +0900

    [D3] Title: [S/W 문제해결 기본] 7일차 - 암호생성기, Time: 229 ms, Memory: 30,628 KB -BaekjoonHub

--- SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
+++ SWEA/D3/1225. ［S／W 문제해결 기본］ 7일차 － 암호생성기/［S／W 문제해결 기본］ 7일차 － 암호생성기.java
@@ -2,53 +2,51 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
+import java.util.Arrays;
 import java.util.Deque;
 
 class Solution {
 
     static Deque<Integer> dq;
-    static int SIZE = 8;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         for (int t = 1; t <= 10; t++) {
-            sb.append("#" + t + " ");
-            br.readLine();
+            t = Integer.parseInt(br.readLine());
+            initQue(br);
 
-            String[] inputArr = br.readLine().split(" ");
-            dq = new ArrayDeque<>();
-            for (int i = 0; i < SIZE; i++) {
-                dq.offer(Integer.parseInt(inputArr[i]));
+            int minus = 1;
+            while (true) {
+                int poll = dq.poll();
+                if (poll - minus <= 0) {
+                    dq.offer(0);
+                    break;
+                }
+                dq.offer(poll - minus++);
+                if (minus > 5) {
+                    minus = 1;
+                }
             }
-            sb.append(answer() + "\n");
+
+            sb.append("#").append(t).append(" ");
+            for (int num : dq) {
+                sb.append(num).append(" ");
+            }
+            sb.append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static String answer() {
-        int minus = 1;
-        while (true) {
-            if (dq.peekLast() <= 0) { // 종료조건
-                dq.pollLast();
-                dq.offer(0);
-                return getStringResult();
-            }
-            dq.offer(dq.poll() - minus++);
-            if (minus > 5) {
-                minus = 1;
-            }
+    private static void initQue(BufferedReader br) throws IOException {
+        int[] inputs = Arrays.stream(br.readLine().split(" "))
+                .mapToInt(Integer::valueOf)
+                .toArray();
+        dq = new ArrayDeque<>();
+        for (int num : inputs) {
+            dq.offer(num);
         }
     }
-
-    static String getStringResult() {
-        StringBuilder result = new StringBuilder();
-        for(int value : dq) {
-            result.append(value + " ");
-        }
-        return result.toString();
-    }
-
 }
