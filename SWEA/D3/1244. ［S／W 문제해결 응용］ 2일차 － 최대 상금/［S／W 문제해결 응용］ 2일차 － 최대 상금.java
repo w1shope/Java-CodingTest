58288723a0839ecdc3307b5e4dd1a13c01564629
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-11 20:21:18 +0900

    [D3] Title: [S/W 문제해결 응용] 2일차 - 최대 상금, Time: 145 ms, Memory: 25,112 KB -BaekjoonHub

--- SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
+++ SWEA/D3/1244. ［S／W 문제해결 응용］ 2일차 － 최대 상금/［S／W 문제해결 응용］ 2일차 － 최대 상금.java
@@ -6,53 +6,62 @@ import java.util.Set;
 
 class Solution {
 
-    static int result;
-    static Set<String> visited;
+    static int K; // 최대 교환 횟수
+    static int answer = Integer.MIN_VALUE;
+    static Set<Integer> visited; // 방문 여부
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
+
         int T = Integer.parseInt(br.readLine());
         for (int t = 1; t <= T; t++) {
-            sb.append("#" + t + " ");
-            String[] inputArr = br.readLine().split(" ");
-            String num = inputArr[0]; // 입력 숫자
-            int maxChangeCount = Integer.parseInt(inputArr[1]); // 반드시 해야하는 교환 횟수
-
-            result = 0;
+            String[] inputs = br.readLine().split(" ");
+            char[] arr = inputs[0].toCharArray();
+            K = Integer.parseInt(inputs[1]);
+            answer = 0;
             visited = new HashSet<>();
 
-            dfs(num, 0, maxChangeCount);
-            sb.append(result + "\n");
+            dfs(arr, 0);
+            sb.append("#" + t + " ").append(answer + "\n");
         }
 
         System.out.println(sb);
     }
 
-    static void dfs(String num, int changeCount, int maxChangeCount) {
-        if (changeCount == maxChangeCount) { // 교환 횟수가 꽉찼다면
-            result = Math.max(result, Integer.parseInt(num));
+    static void dfs(char[] arr, int changeCount) {
+        if (changeCount == K) {
+            int money = charToInteger(arr);
+            answer = Math.max(answer, money);
             return;
         }
 
-        if (visited.contains(num)) {
+        // 이미 자리 교체를 완료한 숫자는 다시 탐색 X
+        if (visited.contains(charToInteger(arr))) {
             return;
         }
+        visited.add(charToInteger(arr));
 
-        visited.add(num);
-        for (int i = 0; i < num.length() - 1; i++) {
-            for (int j = i + 1; j < num.length(); j++) {
-                String swapStr = swap(num, i, j); // i <-> j 자리 바꾸기
-                dfs(swapStr, changeCount + 1, maxChangeCount);
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if (i == j) {
+                    continue;
+                }
+                swap(arr, i, j);
+                dfs(arr, changeCount + 1);
+                swap(arr, j, i);
             }
         }
     }
 
-    static String swap(String num, int idx1, int idx2) {
-        char[] arr = num.toCharArray();
-        char tmp = arr[idx1];
-        arr[idx1] = arr[idx2];
-        arr[idx2] = tmp;
-        return new String(arr);
+    static int charToInteger(char[] arr) {
+        return Integer.parseInt(new String(arr));
     }
+
+    static void swap(char[] arr, int i, int j) {
+        char tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
 }
