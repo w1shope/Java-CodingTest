569952c2c2eaa55a5a166b429616b625e68910ac
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-12 11:33:18 +0900

    [D3] Title: [S/W 문제해결 기본] 3일차 - String, Time: 105 ms, Memory: 18,652 KB -BaekjoonHub

--- SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
+++ SWEA/D3/1213. ［S／W 문제해결 기본］ 3일차 － String/［S／W 문제해결 기본］ 3일차 － String.java
@@ -3,34 +3,32 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 class Solution {
-
-    static String findStr;
-    static String str;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-        while (true) {
-            int t = Integer.parseInt(br.readLine());
-            findStr = br.readLine(); // 찾고자 하는 문자열
-            str = br.readLine(); // 원본 문자열
 
-            sb.append("#" + t + " " + answer() + "\n");
-            if (t == 10) {
-                break;
-            }
+        for (int t = 1; t <= 10; t++) {
+            t = Integer.parseInt(br.readLine());
+            String find = br.readLine();
+            String input = br.readLine();
+            sb.append("#" + t + " " + answer(input, find)).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static int answer() {
-        int count = 0;
-        while (str.indexOf(findStr) != -1) {
-            int index = str.indexOf(findStr);
-            count++;
-            str = str.substring(index + 1);
+    static int answer(String input, String find) {
+        int answer = 0;
+        while (true) {
+            int idx = input.indexOf(find);
+            if (idx == -1) {
+                break;
+            }
+
+            answer++;
+            input = input.substring(idx + find.length());
         }
-        return count;
+        return answer;
     }
+
 }
