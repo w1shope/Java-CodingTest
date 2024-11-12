7d2080d28fe0cbcab14c874f56d70f5ddcd9d1d4
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-12 12:11:01 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - Flatten, Time: 124 ms, Memory: 19,964 KB -BaekjoonHub

--- SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
+++ SWEA/D3/1208. ［S／W 문제해결 기본］ 1일차 － Flatten/［S／W 문제해결 기본］ 1일차 － Flatten.java
@@ -1,34 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 class Solution {
 
-    static int dumpCount;
-    static int[] buildings;
+    static final int SIZE = 100;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
         for (int t = 1; t <= 10; t++) {
-            sb.append("#" + t + " ");
-            dumpCount = Integer.parseInt(br.readLine());
-            buildings = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::valueOf)
-                    .toArray();
+            int n = Integer.parseInt(br.readLine());
 
-            while (dumpCount != 0) {
-                Arrays.sort(buildings);
-                buildings[0]++;
-                buildings[99]--;
-                dumpCount--;
+            PriorityQueue<Integer> min = new PriorityQueue<>();
+            PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+
+            String[] inputs = br.readLine().split(" ");
+            for (int i = 0; i < SIZE; i++) {
+                int value = Integer.parseInt(inputs[i]);
+                min.add(value);
+                max.add(value);
             }
-            
-            Arrays.sort(buildings);
 
-            sb.append((buildings[99] - buildings[0]) + "\n");
+            for (int i = 0; i < n; i++) {
+                int minValue = min.poll();
+                min.add(minValue + 1);
+
+                int maxValue = max.poll();
+                max.add(maxValue - 1);
+            }
+
+            sb.append("#" + t + " ").append(max.peek() - min.peek()).append("\n");
         }
 
         System.out.println(sb);
