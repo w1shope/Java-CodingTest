c0096d500273ca0b0e6b80b5f023341c1bb442ea
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-12 12:31:30 +0900

    [D3] Title: [S/W 문제해결 기본] 1일차 - View, Time: 132 ms, Memory: 22,860 KB -BaekjoonHub

--- SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
+++ SWEA/D3/1206. ［S／W 문제해결 기본］ 1일차 － View/［S／W 문제해결 기본］ 1일차 － View.java
@@ -1,40 +1,50 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 
 class Solution {
+
+    static int N;
+    static int[] buildings;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
 
         for (int t = 1; t <= 10; t++) {
-            sb.append("#" + t + " ");
-            int n = Integer.parseInt(br.readLine());
-            int[] buildings = Arrays.stream(br.readLine().split(" "))
-                    .mapToInt(Integer::valueOf)
-                    .toArray();
-            int result = 0;
-            for (int i = 2; i < n - 2; i++) {
-                result += getViewCount(buildings, i);
+            N = Integer.parseInt(br.readLine()); // 건물 수
+            buildings = new int[N];
+            String[] inputs = br.readLine().split(" ");
+            for (int i = 0; i < N; i++) {
+                buildings[i] = Integer.parseInt(inputs[i]);
             }
-            sb.append(result + "\n");
+
+            sb.append("#" + t + " ").append(answer() + "\n");
         }
 
         System.out.println(sb);
-
     }
 
-    static int getViewCount(int[] buildings, int idx) {
-        int maxBuildingHeight = Integer.MIN_VALUE;
-        for (int i = idx - 2; i <= idx + 2; i++) {
-            if (i != idx) {
-                if (buildings[i] >= buildings[idx]) {
-                    return 0;
+    static int answer() {
+        int answer = 0;
+        for (int i = 2; i < N - 2; i++) {
+            int nowBuildingHeight = buildings[i];
+            int viewCount = Integer.MAX_VALUE;
+            for (int j = i - 2; j <= i + 2; j++) {
+                if (i == j) { // 같읕 건물
+                    continue;
                 }
-                maxBuildingHeight = Math.max(maxBuildingHeight, buildings[i]);
+                if (buildings[j] > nowBuildingHeight) { // 주변 건물이 더 크다면
+                    viewCount = -1;
+                    break;
+                }
+                // 확보한 조경 갱신
+                viewCount = Math.min(viewCount, nowBuildingHeight - buildings[j]);
+            }
+            if (viewCount != -1) { // 주변 건물이 더 크면
+                answer += viewCount;
             }
         }
-        return buildings[idx] - maxBuildingHeight;
+        return answer;
     }
 }
