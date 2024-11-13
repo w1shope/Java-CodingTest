816b475259b77cb3897ef562dc71c9d79a8dc5d0
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-13 21:25:38 +0900

    [Silver III] Title: N번째 큰 수, Time: 1792 ms, Memory: 346240 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,29 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Comparator;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class Main {
-    static Queue<Integer> que = new PriorityQueue<>(Comparator.reverseOrder());
+
+    static int N;
+    static List<Integer> list = new ArrayList<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            for (String s : input)
-                que.offer(Integer.parseInt(s));
-        }
-
-        int count = 0;
-        while (!que.isEmpty()) {
-            int num = que.poll();
-            if (++count == n) {
-                System.out.println(num);
-                break;
+        N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            String[] inputs = br.readLine().split(" ");
+            for (String input : inputs) {
+                list.add(Integer.parseInt(input));
             }
         }
+
+        Collections.sort(list, Collections.reverseOrder());
+
+        System.out.println(list.get(N - 1));
     }
 }
