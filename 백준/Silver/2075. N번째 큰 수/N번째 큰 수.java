c8f8dda937bb8c2332df42b48dff505dae8e91b9
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-13 21:35:38 +0900

    [Silver III] Title: N번째 큰 수, Time: 756 ms, Memory: 255944 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -1,27 +1,30 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.PriorityQueue;
 
 public class Main {
 
     static int N;
-    static List<Integer> list = new ArrayList<>();
+    static PriorityQueue<Integer> pq = new PriorityQueue<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
+
         for (int i = 0; i < N; i++) {
             String[] inputs = br.readLine().split(" ");
             for (String input : inputs) {
-                list.add(Integer.parseInt(input));
+                int num = Integer.parseInt(input);
+                if (pq.size() < N) {
+                    pq.offer(num); // N개의 숫자를 채우지 못했다면
+                } else if (num > pq.peek()) { // N개를 채웠을 때, 더 큰 숫자가 입력되었다면
+                    pq.poll();
+                    pq.offer(num);
+                }
             }
         }
 
-        Collections.sort(list, Collections.reverseOrder());
-
-        System.out.println(list.get(N - 1));
+        System.out.println(pq.peek());
     }
 }
