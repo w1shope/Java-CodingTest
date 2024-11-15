dbe0bfc5be0f8e328e9c7bb25243f6cff77ffb6a
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-15 20:54:05 +0900

    [Silver V] Title: 종이자르기, Time: 104 ms, Memory: 14072 KB -BaekjoonHub

--- 백준/Silver/2628. 종이자르기/종이자르기.java
+++ 백준/Silver/2628. 종이자르기/종이자르기.java
@@ -1,59 +1,58 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.LinkedList;
-import java.util.PriorityQueue;
-import java.util.Queue;
 
 public class Main {
 
     static int N, M;
-    static Queue<Integer> rowsQue = new LinkedList<>(); // 0
-    static Queue<Integer> colsQue = new LinkedList<>(); // 1
-    static PriorityQueue<Integer> rowsPr = new PriorityQueue<>();
-    static PriorityQueue<Integer> colsPr = new PriorityQueue<>();
+    static int[] rows;
+    static int[] cols;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inputs = br.readLine().split(" ");
         N = Integer.parseInt(inputs[0]); // 열 개수
         M = Integer.parseInt(inputs[1]); // 행 개수
+        rows = new int[M + 1];
+        cols = new int[N + 1];
 
         int k = Integer.parseInt(br.readLine());
         while (k-- > 0) {
             inputs = br.readLine().split(" ");
-            int a = Integer.parseInt(inputs[0]);
-            int b = Integer.parseInt(inputs[1]);
-            if (a == 0) {
-                rowsPr.offer(b);
-            } else {
-                colsPr.offer(b);
+            int rowOrCol = Integer.parseInt(inputs[0]);
+            int idx = Integer.parseInt(inputs[1]);
+            if (rowOrCol == 0) { // 행 자르기
+                rows[idx] = 1;
+            } else { // 열 자르기
+                cols[idx] = 1;
             }
         }
 
-        while (!rowsPr.isEmpty()) {
-            rowsQue.offer(rowsPr.poll());
-        }
-        rowsQue.offer(M);
-
-        while (!colsPr.isEmpty()) {
-            colsQue.offer(colsPr.poll());
-        }
-        colsQue.offer(N);
+        // 마지막 행, 열을 포함한 종이 크기를 고려한다.
+        rows[M] = 1;
+        cols[N] = 1;
 
         System.out.println(answer());
     }
 
     static int answer() {
         int answer = 0;
+
         int prevRowIdx = 0;
-        while (!rowsQue.isEmpty()) {
-            int nowRowIdx = rowsQue.poll();
+        for (int nowRowIdx = 0; nowRowIdx <= M; nowRowIdx++) {
+            if (rows[nowRowIdx] == 0) { // 행 자르기 기준선이 아니라면
+                continue;
+            }
+
             int prevColIdx = 0;
-            for (int i = 0; i < colsQue.size(); i++) {
-                int nowColIdx = colsQue.poll();
+            for (int nowColIdx = 0; nowColIdx < cols.length; nowColIdx++) {
+                if (cols[nowColIdx] == 0) { // 열 자르기 기준선이 아니라면
+                    continue;
+                }
+
+                // 이전 선(행, 열)과 현재 선(행, 열)로 종이를 만들어서 크기를 구한 후, 
+                // 크기를 비교하여 갱신한다.
                 answer = Math.max(answer, getArea(prevRowIdx, nowRowIdx, prevColIdx, nowColIdx));
-                colsQue.offer(nowColIdx);
                 prevColIdx = nowColIdx;
             }
             prevRowIdx = nowRowIdx;
