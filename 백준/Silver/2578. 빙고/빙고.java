9e9f64d76e33be4b577670ecea784e2455bbdf83
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-16 15:31:54 +0900

    [Silver IV] Title: 빙고, Time: 104 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Silver/2578. 빙고/빙고.java
+++ 백준/Silver/2578. 빙고/빙고.java
@@ -1,103 +1,129 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
 
+    static String[][] numbers;
+    static final int SIZE = 5;
+    static int count; // 사회자가 몇 번째의 수를 불렀는지
+    static Map<String, int[]> map = new HashMap<>();
+    static final int BREAK_BINGO_COUNT = 3;
+
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[][] bingo = new int[5][5];
-        for (int i = 0; i < 5; i++) {
-            String[] input = br.readLine().split(" ");
-            for (int j = 0; j < 5; j++)
-                bingo[i][j] = Integer.parseInt(input[j]);
+        // 빙고 초기화
+        numbers = new String[SIZE][SIZE];
+        for (int i = 0; i < SIZE; i++) {
+            String[] inputs = br.readLine().split(" ");
+            for (int j = 0; j < SIZE; j++) {
+                numbers[i][j] = inputs[j];
+                map.put(inputs[j], new int[]{i, j}); // {y, x}
+            }
         }
 
-        int count = 0;
-        for (int i = 0; i < 5; i++) {
-            String[] input = br.readLine().split(" ");
-            int bingoCount = 0;
-            for (int j = 0; j < input.length; j++) {
-                checkNum(bingo, Integer.parseInt(input[j]));
+        int answer = -1;
+        // 사회자가 5개의 숫자를 5번 부른다.
+        for (int i = 0; i < SIZE; i++) {
+            for (String input : br.readLine().split(" ")) {
+                int[] pos = map.get(input);
+                numbers[pos[0]][pos[1]] = "0"; // 방문 처리
                 count++;
-
-                bingoCount += isDiagonalBingo(bingo);
-                bingoCount += isRowBingo(bingo);
-                bingoCount += isColumnBingo(bingo);
-                if (bingoCount >= 3) {
-                    System.out.println(count);
-                    return;
-                }
-                bingoCount = 0;
-            }
-        }
-    }
-
-    private static void checkNum(int[][] bingo, int num) {
-        for (int i = 0; i < 5; i++) {
-            for (int j = 0; j < 5; j++) {
-                if (bingo[i][j] == num) {
-                    bingo[i][j] = -1;
-                    return;
+                if (isBingo()) {
+                    answer = count;
+                    break;
                 }
             }
-        }
-    }
-
-    private static int isDiagonalBingo(int[][] bingo) {
-        int count = 0;
-
-        int x = 0, y = 0, tmp = 0;
-        while (x < 5 && y < 5) {
-            if (bingo[y][x] != -1)
+            if (answer != -1) {
                 break;
-            x++;
-            y++;
-            tmp++;
-        }
-        if (tmp == 5)
-            count++;
-
-        x = 4;
-        y = 0;
-        tmp = 0;
-        while (true) {
-            if (y > 4 || x < 0)
-                break;
-            if (bingo[y][x] != -1)
-                break;
-            x--;
-            y++;
-            tmp++;
-        }
-        if (tmp == 5)
-            count++;
-
-        return count;
-    }
-
-    private static int isRowBingo(int[][] bingo) {
-        int count = 0;
-        for (int y = 0; y < 5; y++) {
-            if (Arrays.stream(bingo[y]).filter(num -> num == -1).count() == 5)
-                count += 1;
-        }
-        return count;
-    }
-
-    private static int isColumnBingo(int[][] bingo) {
-        int count = 0;
-        for (int y = 0; y < 5; y++) {
-            int tmp = 0;
-            for (int x = 0; x < 5; x++) {
-                if (bingo[x][y] == -1)
-                    tmp += 1;
             }
-            if (tmp == 5)
-                count += 1;
         }
-        return count;
+
+        System.out.println(answer);
+    }
+
+    static boolean isBingo() {
+        int bingoCount = 0;
+
+        // 세 줄의 빙고가 존재하면 종료한다.
+        // 가로
+        for (int y = 0; y < SIZE; y++) {
+            if (isBingo(numbers[y])) {
+                bingoCount++;
+            }
+            if (bingoCount == BREAK_BINGO_COUNT) {
+                return true;
+            }
+        }
+
+        // 세로
+        for (int x = 0; x < SIZE; x++) {
+            if (isBingo(x)) {
+                bingoCount++;
+            }
+            if (bingoCount == BREAK_BINGO_COUNT) {
+                return true;
+            }
+        }
+
+        // 우대각선
+        if (isBingo(true)) {
+            bingoCount++;
+            if (bingoCount == BREAK_BINGO_COUNT) {
+                return true;
+            }
+        }
+
+        // 좌대각선
+        if (isBingo(false)) {
+            bingoCount++;
+            if (bingoCount == BREAK_BINGO_COUNT) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    static boolean isBingo(String[] rowArr) { // 가로 빙고 여부
+        for (int i = 0; i < SIZE; i++) {
+            if (isNotVisited(rowArr[i])) { // 불린 적이 없다면
+                return false;
+            }
+        }
+        return true; // 빙고
+    }
+
+    static boolean isBingo(int x) { //세로 빙고 여부
+        for (int i = 0; i < SIZE; i++) {
+            if (isNotVisited(numbers[i][x])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static boolean isBingo(boolean isRightDiagonal) { // 대각선
+        if (isRightDiagonal) { // 우대각선이라면
+            for (int y = 0; y < SIZE; y++) {
+                if (isNotVisited(numbers[y][y])) {
+                    return false;
+                }
+            }
+        } else { // 좌대각선이라면
+            for (int y = 0; y < SIZE; y++) {
+                if (isNotVisited(numbers[y][SIZE - y - 1])) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    static boolean isNotVisited(String str) {
+        return !str.equals("0");
     }
 }
