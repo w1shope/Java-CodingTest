95f2f08d1f8fac5e0b7dfaad50b832d57733d6e0
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-16 18:38:51 +0900

    [Silver IV] Title: 설탕 배달, Time: 116 ms, Memory: 14680 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -1,25 +1,46 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
+
+    static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
 
-        int n = Integer.parseInt(br.readLine());
-        int sum = 0;
-        while (n > 0) {
-            if (n % 5 == 0) {
-                sum += n / 5;
-                break;
-            }
-            if (n < 3) {
-                sum = -1;
-                break;
-            }
-            n -= 3;
-            sum += 1;
-        }
-        System.out.println(sum);
+        System.out.println(bfs());
     }
+
+    static int bfs() {
+        boolean[] visited = new boolean[N + 1];
+        Queue<int[]> que = new LinkedList<>();
+        que.offer(new int[]{0, 0});
+
+        while (!que.isEmpty()) {
+            int[] poll = que.poll();
+            int kg = poll[0];
+            int count = poll[1];
+
+            if (kg == N) {
+                return count;
+            }
+
+            if (kg > N || visited[kg]) {
+                continue;
+            }
+
+            visited[kg] = true;
+            // 5kg
+            que.offer(new int[]{kg + 5, count + 1});
+
+            // 3kg
+            que.offer(new int[]{kg + 3, count + 1});
+        }
+        return -1;
+    }
+
 }
