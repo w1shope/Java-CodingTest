38fd4a39505387a80e895e15b119773c89364cac
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-16 11:37:12 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - Magnetic, Time: 161 ms, Memory: 36,268 KB -BaekjoonHub

--- SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
+++ SWEA/D3/1220. ［S／W 문제해결 기본］ 5일차 － Magnetic/［S／W 문제해결 기본］ 5일차 － Magnetic.java
@@ -8,9 +8,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
 
         for (int t = 1; t <= 10; t++) {
-            int n = Integer.parseInt(br.readLine()); // 100 고정
-            int[][] arr = new int[n][n];
-
+            int n = Integer.parseInt(br.readLine());
+            int[][] arr = new int[n][n]; // 1: 빨간, 2 : 파란
             for (int i = 0; i < n; i++) {
                 String[] inputs = br.readLine().split(" ");
                 for (int j = 0; j < n; j++) {
@@ -18,22 +17,22 @@ class Solution {
                 }
             }
 
-            int answer = 0;
+            int count = 0;
             for (int x = 0; x < n; x++) {
-                boolean flag = false;
+                boolean isRed = false;
                 for (int y = 0; y < n; y++) {
-                    if (arr[y][x] == 1) { // N극 자성체
-                        flag = true;
-                    } else if (arr[y][x] == 2 && flag) {
-                        answer++;
-                        flag = false;
+                    if (arr[y][x] == 0) {
+                        continue;
                     }
+
+                    if (isRed && arr[y][x] == 2) {
+                        count++;
+                    }
+                    isRed = arr[y][x] == 1 ? true : false;
                 }
             }
-
-            sb.append("#").append(t).append(" ").append(answer).append("\n");
+            sb.append("#" + t + " " + count + "\n");
         }
-
         System.out.println(sb);
     }
 }
