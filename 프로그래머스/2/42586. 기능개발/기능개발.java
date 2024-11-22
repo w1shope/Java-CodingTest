297ae2a4fe50791e1c34cd57755c3390d19640fd
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-11-22 16:13:57 +0900

    [level 2] Title: 기능개발, Time: 5.61 ms, Memory: 74.2 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -5,37 +5,26 @@ import java.util.Queue;
 
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<int[]> que = new LinkedList<>();
-        for (int i = 0; i < progresses.length; i++) {
-            que.offer(new int[]{progresses[i], speeds[i], i + 1});
-        }
-        boolean[] flag = new boolean[que.size() + 1];
-        flag[0] = true;
-
         List<Integer> result = new ArrayList<>();
-        while (!que.isEmpty()) {
-            int[] now = que.poll();
-            int progress = now[0];
-            int speed = now[1];
-            int order = now[2];
 
-            if (flag[order - 1] && progress >= 100) {
-                int count = 1;
-                flag[order] = true;
-                while (!que.isEmpty()) {
-                    if (que.peek()[0] >= 100) {
-                        int[] next = que.poll();
-                        count++;
-                        flag[next[2]] = true;
-                    } else {
-                        break;
-                    }
-                }
-                result.add(count);
+        int[] days = new int[progresses.length];
+        for (int i = 0; i < progresses.length; i++) {
+            days[i] = (int) Math.ceil((100.0 - progresses[i]) / speeds[i]);
+        }
+
+        int maxDay = days[0];
+        int count = 1;
+        for (int i = 1; i < days.length; i++) {
+            if (days[i] <= maxDay) {
+                count++;
             } else {
-                que.offer(new int[]{progress + speed, speed, order});
+                result.add(count);
+                count = 1;
+                maxDay = days[i];
             }
         }
+
+        result.add(count);
 
         return result.stream().mapToInt(Integer::valueOf).toArray();
     }
