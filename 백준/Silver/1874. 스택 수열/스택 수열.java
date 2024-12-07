0eb52208c2a30e486736769563d762e3cdce33de
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-07 13:04:06 +0900

    [Silver II] Title: 스택 수열, Time: 292 ms, Memory: 27728 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -4,44 +4,61 @@ import java.io.InputStreamReader;
 import java.util.Stack;
 
 public class Main {
-
-    static int N;
-    static boolean[] visited;
-    static Stack<Integer> stack;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        N = Integer.parseInt(br.readLine());
-        visited = new boolean[N + 1];
-        stack = new Stack<>();
+        int n = Integer.parseInt(br.readLine());
 
         StringBuilder sb = new StringBuilder();
-        int prev = 0;
-        for (int i = 1; i <= N; i++) {
-            int now = Integer.parseInt(br.readLine());
-            if (now > prev) {
+
+        Stack<Integer> st = new Stack<>();
+        boolean[] popFlag = new boolean[n + 1]; // pop 여부
+
+        // 첫 번째 입력
+        int now = Integer.parseInt(br.readLine());
+        for (int i = 1; i <= now; i++) {
+            st.push(i);
+            sb.append("+\n");
+        }
+        int prev = st.pop(); // 이전 입력
+        popFlag[prev] = true;
+        sb.append("-\n");
+
+        boolean isBreak = false;
+        for (int i = 1; i < n; i++) {
+            now = Integer.parseInt(br.readLine());
+
+            // 스택이 비어있거나, 이미 출력되었거나
+            if (popFlag[now]) {
+                isBreak = true;
+                break;
+            }
+
+            // push : 이전 입력 < 현재 입력
+            if (prev < now) {
                 for (int j = prev + 1; j <= now; j++) {
-                    stack.push(j);
+                    st.push(j);
                     sb.append("+\n");
                 }
-                prev = now;
-                stack.pop();
+                prev = st.pop();
+                popFlag[prev] = true;
                 sb.append("-\n");
-            } else {
+            } else { // pop : 이전 입력 > 현재 입력
+                if (st.isEmpty()) { // pop할 원소가 없을 때
+                    isBreak = true;
+                    break;
+                }
+
                 while (true) {
-                    if (stack.isEmpty()) {
-                        System.out.println("NO");
-                        return;
-                    }
+                    int pop = st.pop();
+                    popFlag[pop] = true;
                     sb.append("-\n");
-                    int poll = stack.pop();
-                    if (poll == now) {
+                    if (pop == now) {
                         break;
                     }
                 }
             }
         }
 
-        System.out.println(sb);
+        System.out.println(isBreak ? "NO" : sb);
     }
 }
