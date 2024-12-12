f8ad9ed21c782400b5bb96270da20f8a920c06aa
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-12 15:10:58 +0900

    [Silver V] Title: 큰 수 구성하기, Time: 128 ms, Memory: 15304 KB -BaekjoonHub

--- 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
+++ 백준/Silver/18511. 큰 수 구성하기/큰 수 구성하기.java
@@ -16,46 +16,38 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String[] inputs = br.readLine().split(" ");
         N = Integer.parseInt(inputs[0]);
-        K = Integer.parseInt(inputs[1]);
 
+        K = Integer.parseInt(inputs[1]);
         for (String input : br.readLine().split(" ")) {
             numbers.add(Integer.parseInt(input));
         }
 
-        for (int num : numbers) {
-            String selectedNum = "" + num;
-            dfs(selectedNum, 0, new HashSet<>());
+        // {숫자합, 선택할 숫자, 방문 여부}
+        for (int i = 0; i < K; i++) {
+            dfs(numbers.get(i) + "", 0, new HashSet<>());
         }
 
         System.out.println(answer);
     }
 
-    static void dfs(String number, int selectedIdx, Set<String> visited) {
-        if (Integer.parseInt(number) > N) { // N보다 크면 안 된다.
+    static void dfs(String number, int idx, Set<String> visited) {
+        // 숫자 합 > N or 이미 탐색한 숫자 or 인덱스 초과
+        if (Integer.parseInt(number) > N || visited.contains(number) || idx >= K) {
             return;
         }
 
-        if (visited.contains(number)) { // 중복 연산을 수행하지 않는다.
-            return;
-        }
-        
+        // 최대값 갱신
         answer = Math.max(answer, Integer.parseInt(number));
 
-        if (selectedIdx >= K) { // 인덱스 범위를 벗어나면 안 된다.
-            return;
-        }
-
-        // 현재 숫자를 한 번 더 선택한다.
+        // 현재 숫자를 선택하되, 또다시 선택한다.
         visited.add(number);
-        dfs(number + numbers.get(selectedIdx), selectedIdx, visited);
-        visited.remove(number);
+        dfs(number + numbers.get(idx), idx, visited);
 
-        // 현재 숫자를 선택 후 다음 숫자를 선택한다.
-        visited.add(number);
-        dfs(number + numbers.get(selectedIdx), selectedIdx + 1, visited);
-        visited.remove(number);
+        //현재 숫자를 선택하되, 다음 숫자를 선택한다.
+        dfs(number + numbers.get(idx), idx + 1, visited);
 
-        // 현재 숫자를 선택하지 않고 다음 숫자를 선택한다.
-        dfs(number, selectedIdx + 1, visited);
+        // 현재 숫자를 선택하지 않는다.
+        visited.remove(number);
+        dfs(number, idx + 1, visited);
     }
 }
