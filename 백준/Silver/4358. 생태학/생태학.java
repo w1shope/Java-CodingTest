b72411088722160d6cd8c5a45bc127bdc1e12624
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-14 13:21:38 +0900

    [Silver II] Title: 생태학, Time: 712 ms, Memory: 96896 KB -BaekjoonHub

--- 백준/Silver/4358. 생태학/생태학.java
+++ 백준/Silver/4358. 생태학/생태학.java
@@ -1,39 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
-import java.util.HashSet;
+import java.util.List;
 import java.util.Map;
-import java.util.PriorityQueue;
-import java.util.Set;
 
 public class Main {
-
-    static Map<String, Integer> map = new HashMap<>();
-    static Set<String> set = new HashSet<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int size = 0;
 
+        Map<String, Integer> map = new HashMap<>(); // {이름, 입력 횟수}
+        List<String> names = new ArrayList<>(); // 이름들
+
+        // 입력이 종료될 때까지 입력
+        int mod = 0; // 입력된 생물 개수
         while (true) {
             String input = br.readLine();
+
+            // 아무 값도 입력되지 않는다면 종료
             if (input == null) {
                 break;
             }
-            map.put(input, map.getOrDefault(input, 0) + 1);
-            set.add(input);
-            size++;
-        }
-        PriorityQueue<String> que = new PriorityQueue<>(set);
 
+            // 입력 정보 저장
+            mod++;
+            if (map.containsKey(input)) {
+                map.put(input, map.get(input) + 1);
+            } else {
+                names.add(input);
+                map.put(input, 1);
+            }
+        }
+
+        // 이름을 사전 순으로 정렬
+        Collections.sort(names);
+
+        // 사전 순으로, 이름과 차지하는 비율 출력
         StringBuilder sb = new StringBuilder();
-        while (!que.isEmpty()) {
-            String name = que.poll();
-            double percent = ((double) map.get(name) / size) * 100;
-            sb.append(String.format("%s %.4f\n", name, percent));
+        for (String name : names) {
+            sb.append(String.format("%s %.4f\n", name, ((double) map.get(name) / mod) * 100));
         }
-
         System.out.println(sb);
     }
 }
