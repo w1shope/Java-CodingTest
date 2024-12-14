7702613b0de371986ba4d4e206d2ae49269b2c2d
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-14 12:45:43 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 492 ms, Memory: 57388 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.java
@@ -1,64 +1,38 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class Main {
-
-    static int N, M;
-    static Map<Integer, String> numbersMap;
-    static Map<String, Integer> stringMap;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int[] inputArr = Arrays.stream(br.readLine().split(" "))
-                .mapToInt(Integer::valueOf)
-                .toArray();
-        N = inputArr[0]; // 포켓몬 개수
-        M = inputArr[1]; // 몇 번 물어볼건지
+        String[] inputs = br.readLine().split(" ");
 
-        numbersMap = new HashMap<>();
-        stringMap = new HashMap<>();
+        // N, M 입력
+        int n = Integer.parseInt(inputs[0]);
+        int m = Integer.parseInt(inputs[1]);
 
-        for (int i = 1; i <= N; i++) {
-            String input = br.readLine();
-            numbersMap.put(i, input);
-            stringMap.put(input, i);
+        // 도감 정보 저장
+        Map<String, String> alphabets = new HashMap<>(); // {포켓몬 이름, 포켓몬 번호}
+        Map<String, String> numbers = new HashMap<>(); // {포켓몬 번호, 포켓몬 이름}
+        for (int i = 1; i <= n; i++) {
+            String name = br.readLine();
+            alphabets.put(name, i + "");
+            numbers.put(i + "", name);
         }
 
+        // m개의 입력을 받아 출력
         StringBuilder sb = new StringBuilder();
-        for (int i = 0; i < M; i++) {
+        for (int i = 0; i < m; i++) {
             String input = br.readLine();
-            sb.append(answer(input) + "\n");
+            // 포켓몬 이름을 입력했다면 포켓몬의 번호를 출력하고,
+            if (Character.isAlphabetic(input.charAt(0))) {
+                sb.append(alphabets.get(input)).append("\n");
+            } else { // 포켓몬의 번호를 입력했다면 포켓몬의 이름을 출력한다.
+                sb.append(numbers.get(input)).append("\n");
+            }
         }
-
         System.out.println(sb);
-    }
-
-    static String answer(String str) {
-        if (isDigit(str)) { // 입력이 숫자라면
-            int number = Integer.parseInt(str);
-            return numbersMap.get(number);
-        }
-
-        // 입력이 문자열이라면
-        return String.valueOf(stringMap.get(str));
-    }
-
-    static boolean isDigit(String str) {
-        int num = str.charAt(0) - '0';
-        return 0 <= num && num <= 9;
-    }
-
-    static class PocketMon {
-        int number; // 입력 순서
-        String name; // 이름
-
-        public PocketMon(int number, String name) {
-            this.number = number;
-            this.name = name;
-        }
     }
 }
