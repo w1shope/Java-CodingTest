3a2783a218874860f0443081323d9c3c333be1d3
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-15 15:20:40 +0900

    [Silver IV] Title: 베스트셀러, Time: 104 ms, Memory: 14320 KB -BaekjoonHub

--- 백준/Silver/1302. 베스트셀러/베스트셀러.java
+++ 백준/Silver/1302. 베스트셀러/베스트셀러.java
@@ -1,28 +1,47 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
-import java.util.stream.Collectors;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    private static Map<String, Integer> map = new HashMap<>();
-    
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int n = Integer.parseInt(br.readLine());
+        Map<String, Book> map = new HashMap<>(); // {책 이름, 인스턴스}
         for (int i = 0; i < n; i++) {
-            String input = br.readLine();
-            map.put(input, map.getOrDefault(input, 0) + 1);
+            String name = br.readLine();
+            if (map.containsKey(name)) {
+                Book book = map.get(name);
+                book.sellCount++;
+            } else {
+                map.put(name, new Book(name, 1));
+            }
         }
 
-        Set<Map.Entry<String, Integer>> entries = map.entrySet();
-        List<Map.Entry<String, Integer>> lists = entries.stream().sorted((e1, e2) -> {
-            if (e1.getValue() == e2.getValue())
-                return e1.getKey().compareTo(e2.getKey());
-            return Integer.compare(e2.getValue(), e1.getValue());
-        }).collect(Collectors.toList());
+        List<Book> books = new ArrayList<>(map.values());
+        Collections.sort(books, (a, b) -> {
+            if (a.sellCount == b.sellCount) {
+                return a.name.compareTo(b.name);
+            }
+            return b.sellCount - a.sellCount;
+        });
 
-        System.out.println(lists.get(0).getKey());
+        System.out.println(books.get(0).name);
+
+    }
+
+    static class Book {
+        String name;
+        int sellCount;
+
+        public Book(String name, int sellCount) {
+            this.name = name;
+            this.sellCount = sellCount;
+        }
     }
 }
