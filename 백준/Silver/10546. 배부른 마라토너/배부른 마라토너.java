3b71633fdff7c37dcb83c8561918bdeebbbd863d
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-15 15:12:39 +0900

    [Silver IV] Title: 배부른 마라토너, Time: 356 ms, Memory: 32536 KB -BaekjoonHub

--- 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
+++ 백준/Silver/10546. 배부른 마라토너/배부른 마라토너.java
@@ -1,9 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
@@ -12,11 +10,9 @@ public class Main {
         int n = Integer.parseInt(br.readLine());
 
         Map<String, Integer> map = new HashMap<>();
-        List<String> names = new ArrayList<>();
         for (int i = 0; i < n; i++) {
             String name = br.readLine();
             map.put(name, map.getOrDefault(name, 0) + 1);
-            names.add(name);
         }
 
         for (int i = 0; i < n - 1; i++) {
@@ -24,9 +20,9 @@ public class Main {
             map.put(name, map.get(name) - 1);
         }
 
-        for (String name : names) {
-            if (map.get(name) >= 1) {
-                System.out.println(name);
+        for (String key : map.keySet()) {
+            if (map.get(key) >= 1) {
+                System.out.println(key);
                 break;
             }
         }
