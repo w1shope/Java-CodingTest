dcc061f2e3dce9010be944a65afa4c0de84313f5
Heemang Lee (이희망) <wishhopelee@gmail.com>
2024-12-22 13:32:42 +0900

    [Silver IV] Title: 카드2, Time: 172 ms, Memory: 28968 KB -BaekjoonHub

--- 백준/Silver/2164. 카드2/카드2.java
+++ 백준/Silver/2164. 카드2/카드2.java
@@ -1,22 +1,33 @@
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
-        Queue<Integer> que = new LinkedList<>();
+        if (n == 1) {
+            System.out.println(1);
+            return;
+        }
+
+        Deque<Integer> que = new ArrayDeque<>();
         for (int i = 1; i <= n; i++) {
             que.offer(i);
         }
 
-        while (que.size() != 1) {
-            que.poll();
+        while (!que.isEmpty()) {
+            que.pollFirst();
+            if (que.size() == 1) {
+                break;
+            }
+
             que.offer(que.poll());
         }
 
-        System.out.println(que.poll());
+        System.out.print(que.poll());
     }
 }
