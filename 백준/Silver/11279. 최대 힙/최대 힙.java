7b6417355a1bbb01d63d3dded91e5106849f369f
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-11 15:04:08 +0900

    [Silver II] Title: 최대 힙, Time: 296 ms, Memory: 27168 KB -BaekjoonHub

--- 백준/Silver/11279. 최대 힙/최대 힙.java
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -1,36 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Collections;
 import java.util.PriorityQueue;
 
 public class Main {
-
-    static int N;
-    static PriorityQueue<Integer> que = new PriorityQueue<>((a, b) -> b - a); // 내림차순 정렬
-    static StringBuilder sb = new StringBuilder();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < N; i++) {
-            int input = Integer.parseInt(br.readLine());
-            if (input == 0) {
-                print(); // Queue에 저장된 가장 작은 숫자 출력
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
             } else {
-                que.offer(input);
+                pq.offer(num);
             }
         }
 
-        System.out.println(sb.toString());
-    }
-
-    static void print() {
-        if (que.isEmpty()) {
-            sb.append("0\n");
-        } else {
-            sb.append(que.poll() + "\n");
-        }
+        System.out.print(sb);
     }
 }
