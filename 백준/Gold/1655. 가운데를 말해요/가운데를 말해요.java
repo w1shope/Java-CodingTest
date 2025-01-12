569a37b9b74291affd021259ea6bbee473a837f4
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-12 10:51:32 +0900

    [Gold II] Title: 가운데를 말해요, Time: 416 ms, Memory: 33304 KB -BaekjoonHub

--- 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
+++ 백준/Gold/1655. 가운데를 말해요/가운데를 말해요.java
@@ -8,45 +8,31 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-
         int n = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
+        PriorityQueue<Integer> min = new PriorityQueue<>();
+        PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
+
         for (int i = 0; i < n; i++) {
             int num = Integer.parseInt(br.readLine());
 
-            // 값 삽입
-            if (minHeap.size() == maxHeap.size()) {
-                maxHeap.add(num);
+            if (min.size() == max.size()) {
+                max.offer(num);
             } else {
-                minHeap.add(num);
+                min.offer(num);
             }
 
-            // root 비교
-            if (!minHeap.isEmpty()) {
-                if (maxHeap.peek() > minHeap.peek()) {
-                    swap(minHeap, maxHeap);
+            if (!min.isEmpty()) {
+                if (max.peek() > min.peek()) {
+                    min.offer(max.poll());
+                    max.offer(min.poll());
                 }
             }
 
-            // 중간값 출력
-            if (i % 2 == 0) { // 짝수
-                if (minHeap.isEmpty()) {
-                    sb.append(maxHeap.peek()).append("\n");
-                    continue;
-                }
-                sb.append(minHeap.peek() > maxHeap.peek() ? maxHeap.peek() : minHeap.peek()).append("\n");
-            } else { // 홀수
-                sb.append(maxHeap.peek()).append("\n");
-            }
+            sb.append(max.peek()).append("\n");
         }
 
-        System.out.println(sb);
-    }
-
-    static void swap(PriorityQueue<Integer> heap1, PriorityQueue<Integer> heap2) {
-        heap1.add(heap2.poll());
-        heap2.add(heap1.poll());
+        System.out.print(sb);
     }
 }
