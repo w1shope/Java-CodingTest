7c5ce29402613edbb11e40a242dfdbe1c3c2689e
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-12 11:00:36 +0900

    [Silver IV] Title: 암기왕, Time: 1576 ms, Memory: 264172 KB -BaekjoonHub

--- 백준/Silver/2776. 암기왕/암기왕.java
+++ 백준/Silver/2776. 암기왕/암기왕.java
@@ -1,28 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.Arrays;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int T = Integer.parseInt(br.readLine());
 
+        int T = Integer.parseInt(br.readLine());
         StringBuilder sb = new StringBuilder();
+
         while (T-- > 0) {
-            Set<String> book1 = new HashSet<>();
             int n = Integer.parseInt(br.readLine());
-            for (String input : br.readLine().split(" ")) {
-                book1.add(input);
+
+            int[] arr = new int[n];
+            String[] inputs = br.readLine().split(" ");
+            for (int i = 0; i < n; i++) {
+                arr[i] = Integer.parseInt(inputs[i]);
             }
+            Arrays.sort(arr);
 
             int m = Integer.parseInt(br.readLine());
             for (String input : br.readLine().split(" ")) {
-                sb.append(book1.contains(input) ? 1 : 0).append("\n");
+                sb.append(binarySearch(arr, Integer.parseInt(input)) ? 1 : 0).append("\n");
             }
         }
 
-        System.out.println(sb);
+        System.out.print(sb);
+    }
+
+    static boolean binarySearch(int[] arr, int find) {
+        int left = 0;
+        int right = arr.length - 1;
+        while (left <= right) {
+            int mid = (left + right) / 2;
+            if (find > arr[mid]) {
+                left = mid + 1;
+            } else if (find < arr[mid]) {
+                right = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        return false;
     }
 }
