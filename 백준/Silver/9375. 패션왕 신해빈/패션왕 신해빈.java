2621e77a1a50b8b300e0faa4f6c471274dc58e95
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-12 11:53:18 +0900

    [Silver III] Title: 패션왕 신해빈, Time: 100 ms, Memory: 14192 KB -BaekjoonHub

--- 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
+++ 백준/Silver/9375. 패션왕 신해빈/패션왕 신해빈.java
@@ -1,38 +1,32 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 public class Main {
-
-    static int N;
-    static Map<String, Integer> map;
-    static int result;
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
         while (T-- > 0) {
-            N = Integer.parseInt(br.readLine());
-            map = new HashMap<>();
-            for (int i = 0; i < N; i++) {
-                String[] split = br.readLine().split(" ");
-                String category = split[1]; // 악세서리 카테고리
+            int n = Integer.parseInt(br.readLine());
+
+            Map<String, Integer> map = new HashMap<>();
+            for (int i = 0; i < n; i++) {
+                String[] inputs = br.readLine().split(" ");
+                String category = inputs[1];
                 map.put(category, map.getOrDefault(category, 0) + 1);
             }
 
-            result = 1;
-            for (int count : map.values()) {
-                // count : 카테고리 의상수, 옷을 반드시 입는다고 가정
-                // + 1 : 옷을 입지 않는 경우 고려
-                result *= (count + 1);
+            int count = 1;
+            for (Object obj : map.keySet()) {
+                count *= map.get((String) obj) + 1;
             }
-
-            // 아무 옷도 입지 않은 경우
-            System.out.println(result - 1);
+            sb.append(count - 1).append("\n");
         }
+
+        System.out.println(sb);
     }
 }
