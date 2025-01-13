67e7cdcc89f55383fd5a8e8c4f27be4fe100a207
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-13 13:27:57 +0900

    [Gold III] Title: 싸지방에 간 준하, Time: 932 ms, Memory: 76144 KB -BaekjoonHub

--- 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.java
+++ 백준/Gold/12764. 싸지방에 간 준하/싸지방에 간 준하.java
@@ -4,6 +4,7 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 import java.util.PriorityQueue;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -12,6 +13,7 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         PriorityQueue<Time> times = new PriorityQueue<>();
 
+        // 컴퓨터 시작-종료 시간 저장
         for (int i = 0; i < N; i++) {
             String[] inputs = br.readLine().split(" ");
             int start = Integer.parseInt(inputs[0]);
@@ -19,40 +21,40 @@ public class Main {
             times.offer(new Time(start, end));
         }
 
-        int[] useCount = new int[N];
-        PriorityQueue<int[]> pcQue = new PriorityQueue<>((p1, p2) -> {
-            return p1[1] - p2[1];
-        });
-        boolean[] visited = new boolean[N];
+        int[] useCount = new int[N]; // 컴퓨터별로 사용 횟수
+
+        // 사용 중인 컴퓨터의 (컴퓨터 번호, 종료 시간) 저장
+        // 빨리 종료되는 컴퓨터를 앞에 배치한다.
+        PriorityQueue<int[]> pcQue = new PriorityQueue<>((p1, p2) -> p1[1] - p2[1]);
+
+        // 사용 가능한 컴퓨터 저장
+        TreeSet<Integer> available = new TreeSet<>();
+        for (int i = 0; i < N; i++) {
+            available.add(i);
+        }
+
         Set<Integer> set = new HashSet<>();
-
         while (!times.isEmpty()) {
-            Time current = times.poll();
+            Time current = times.poll(); // 컴퓨터를 사용할 (시작 시간, 종료 시간)
 
+            // 종료 시간이 지난 컴퓨터를 모두 반환한다.
             while (!pcQue.isEmpty()) {
                 if (pcQue.peek()[1] > current.start) {
                     break;
                 }
                 int[] poll = pcQue.poll();
-                visited[poll[0]] = false;
+                available.add(poll[0]); // 사용 가능 상태로 복귀
             }
 
-            int pickIdx = -1;
-            for (int i = 0; i < visited.length; i++) {
-                if (!visited[i]) {
-                    pickIdx = i;
-                    break;
-                }
-            }
-
-            useCount[pickIdx]++;
-            visited[pickIdx] = true;
+            // 사용 가능한 컴퓨터 중 가장 앞의 컴퓨터를 선택
+            int pickIdx = available.pollFirst(); // 사용 가능한 컵퓨터를 가져오되, 컴퓨터 번호가 가장 작은 것을 가져온다.
+            useCount[pickIdx]++; // 현재 컴퓨터의 사용 횟수 + 1
+            pcQue.offer(new int[]{pickIdx, current.end}); // 현재 컴퓨터의 (번호, 종료 시간) 저장
             set.add(pickIdx);
-            pcQue.offer(new int[]{pickIdx, current.end});
         }
 
         StringBuilder sb = new StringBuilder();
-        sb.append(set.size()).append("\n");
+        sb.append(set.size()).append("\n"); // 사용된 컴퓨터의 수
         for (int i = 0; i < useCount.length; i++) {
             if (useCount[i] == 0) {
                 break;
@@ -61,7 +63,6 @@ public class Main {
         }
 
         System.out.println(sb);
-
     }
 
     static class Time implements Comparable<Time> {
