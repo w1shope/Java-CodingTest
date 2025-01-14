01c64b6997e3786a976d1f94a8ce31b720d2c329
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-14 10:13:22 +0900

    [Bronze III] Title: 최소, 최대 2, Time: 852 ms, Memory: 340048 KB -BaekjoonHub

--- 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
+++ 백준/Bronze/20053. 최소， 최대 2/최소， 최대 2.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
@@ -13,16 +11,15 @@ public class Main {
         while (T-- > 0) {
             int n = Integer.parseInt(br.readLine());
 
-            PriorityQueue<Integer> min = new PriorityQueue<>();
-            PriorityQueue<Integer> max = new PriorityQueue<>(Collections.reverseOrder());
-
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
             for (String num : br.readLine().split(" ")) {
                 int val = Integer.parseInt(num);
-                min.offer(val);
-                max.offer(val);
+                min = Math.min(min, val);
+                max = Math.max(max, val);
             }
 
-            sb.append(min.poll()).append(" ").append(max.poll()).append("\n");
+            sb.append(min).append(" ").append(max).append("\n");
         }
 
         System.out.print(sb);
