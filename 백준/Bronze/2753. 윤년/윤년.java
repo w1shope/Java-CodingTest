f2d04abf4ce9a3cc70630263a0dc0122a43747b0
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-14 10:00:31 +0900

    [Bronze V] Title: 윤년, Time: 104 ms, Memory: 14316 KB -BaekjoonHub

--- 백준/Bronze/2753. 윤년/윤년.java
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -1,17 +1,16 @@
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int num = sc.nextInt();
-        if (num % 4 != 0) {
-            System.out.println(0);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        if (num % 4 == 0 && (num % 100 != 0 || num % 400 == 0)) {
+            System.out.print(1);
         } else {
-            if (num % 400 == 0 || num % 100 != 0) {
-                System.out.println(1);
-            } else {
-                System.out.println(0);
-            }
+            System.out.print(0);
         }
     }
 }
