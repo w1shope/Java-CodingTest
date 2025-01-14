d7802bd113a93ba4969621e162e483c49d045a91
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-14 23:05:19 +0900

    [Silver III] Title: 원상 복구 (small), Time: 252 ms, Memory: 58652 KB -BaekjoonHub

--- 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
+++ 백준/Silver/22858. 원상 복구 （small）/원상 복구 （small）.java
@@ -3,34 +3,41 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    static int[] dArr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
 
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
+        String[] inputs = br.readLine().split(" ");
+        int N = Integer.parseInt(inputs[0]);
+        int suffleCount = Integer.parseInt(inputs[1]);
 
-        input = br.readLine().split(" ");
-        int[] s = new int[n];
-        for (int i = 0; i < n; i++)
-            s[i] = Integer.parseInt(input[i]);
-
-        input = br.readLine().split(" ");
-        int[] d = new int[n];
-        for (int i = 0; i < n; i++)
-            d[i] = Integer.parseInt(input[i]) - 1;
-
-        for (int i = 0; i < k; i++) {
-            int[] newS = new int[n];
-            for (int j = 0; j < n; j++) {
-                newS[d[j]] = s[j];
-            }
-            s = newS;
+        int[] arr = new int[N + 1];
+        inputs = br.readLine().split(" ");
+        for (int i = 1; i <= N; i++) {
+            arr[i] = Integer.parseInt(inputs[i - 1]);
         }
 
-        for (int num : s)
-            sb.append(num + " ");
-        System.out.println(sb);
+        dArr = new int[N + 1];
+        inputs = br.readLine().split(" ");
+        for (int i = 1; i <= N; i++) {
+            dArr[i] = Integer.parseInt(inputs[i - 1]);
+        }
+
+        while (suffleCount-- > 0) {
+            int[] temp = new int[N + 1];
+            for (int i = 1; i <= N; i++) {
+                temp[dArr[i]] = arr[i];
+            }
+            arr = temp;
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 1; i <= N; i++) {
+            sb.append(arr[i]).append(" ");
+        }
+
+        System.out.print(sb);
     }
 }
