b36c978079913b120181a03f1b93eea234050eec
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-15 14:43:49 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 488 ms, Memory: 61324 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -8,49 +8,50 @@ import java.util.Set;
 
 public class Main {
 
-    static int N, M;
-    static List<StringBuilder> list = new ArrayList<>();
-    static final StringBuilder init = new StringBuilder("00000000000000000000");
+    static final StringBuilder seats = new StringBuilder("00000000000000000000");
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String[] inputs = br.readLine().split(" ");
-        N = Integer.parseInt(inputs[0]);
-        M = Integer.parseInt(inputs[1]);
+        int N = Integer.parseInt(inputs[0]);
+        int M = Integer.parseInt(inputs[1]);
 
-        for (int i = 0; i <= N; i++) {
-            list.add(new StringBuilder(init));
+        List<StringBuilder> trains = new ArrayList<>();
+        for (int i = 0; i < N; i++) {
+            trains.add(new StringBuilder(seats));
         }
 
         for (int i = 0; i < M; i++) {
             inputs = br.readLine().split(" ");
             int order = Integer.parseInt(inputs[0]);
-            int trainIdx = Integer.parseInt(inputs[1]);
+            int trainIdx = Integer.parseInt(inputs[1]) - 1;
+            int seatIdx = inputs.length == 3 ? Integer.parseInt(inputs[2]) - 1 : 0;
 
-            StringBuilder selectTrain = list.get(trainIdx);
-            if (order == 1) {
-                int seatIdx = Integer.parseInt(inputs[2]) - 1;
-                if (selectTrain.charAt(seatIdx) == '0') {
-                    selectTrain.setCharAt(seatIdx, '1');
-                }
-            } else if (order == 2) {
-                int seatIdx = Integer.parseInt(inputs[2]) - 1;
-                selectTrain.setCharAt(seatIdx, '0');
-            } else if (order == 3) {
-                selectTrain.insert(0, '0');
-                selectTrain.deleteCharAt(20);
-            } else {
-                selectTrain.append("0");
-                selectTrain.deleteCharAt(0);
+            StringBuilder train = trains.get(trainIdx);
+            switch (order) {
+                case 1:
+                    if (train.charAt(seatIdx) == '0') {
+                        train.setCharAt(seatIdx, '1');
+                    }
+                    break;
+                case 2:
+                    train.setCharAt(seatIdx, '0');
+                    break;
+                case 3:
+                    train.insert(0, '0');
+                    train.deleteCharAt(20);
+                    break;
+                default:
+                    train.deleteCharAt(0);
+                    train.append("0");
             }
         }
 
         Set<String> set = new HashSet<>();
-        for (int i = 1; i <= N; i++) {
-            set.add(list.get(i).toString());
+        for (StringBuilder train : trains) {
+            set.add(train.toString());
         }
-
         System.out.println(set.size());
     }
 }
