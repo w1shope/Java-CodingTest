0fa57b49f4c068ceefac4f798fb39a3a26230190
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-15 17:26:41 +0900

    [Silver II] Title: 기차가 어둠을 헤치고 은하수를, Time: 408 ms, Memory: 47424 KB -BaekjoonHub

--- 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
+++ 백준/Silver/15787. 기차가 어둠을 헤치고 은하수를/기차가 어둠을 헤치고 은하수를.java
@@ -1,15 +1,10 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.HashSet;
-import java.util.List;
 import java.util.Set;
 
 public class Main {
-
-    static final StringBuilder seats = new StringBuilder("00000000000000000000");
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
@@ -17,40 +12,30 @@ public class Main {
         int N = Integer.parseInt(inputs[0]);
         int M = Integer.parseInt(inputs[1]);
 
-        List<StringBuilder> trains = new ArrayList<>();
-        for (int i = 0; i < N; i++) {
-            trains.add(new StringBuilder(seats));
-        }
-
-        for (int i = 0; i < M; i++) {
+        int[] trains = new int[N];
+        while (M-- > 0) {
             inputs = br.readLine().split(" ");
             int order = Integer.parseInt(inputs[0]);
             int trainIdx = Integer.parseInt(inputs[1]) - 1;
-            int seatIdx = inputs.length == 3 ? Integer.parseInt(inputs[2]) - 1 : 0;
 
-            StringBuilder train = trains.get(trainIdx);
-            switch (order) {
-                case 1:
-                    if (train.charAt(seatIdx) == '0') {
-                        train.setCharAt(seatIdx, '1');
-                    }
-                    break;
-                case 2:
-                    train.setCharAt(seatIdx, '0');
-                    break;
-                case 3:
-                    train.insert(0, '0');
-                    train.deleteCharAt(20);
-                    break;
-                default:
-                    train.deleteCharAt(0);
-                    train.append("0");
+            if (order == 1) {
+                int seatIdx = Integer.parseInt(inputs[2]) - 1;
+                trains[trainIdx] |= (1 << seatIdx);
+            } else if (order == 2) {
+                int seatIdx = Integer.parseInt(inputs[2]) - 1;
+                trains[trainIdx] &= ~(1 << seatIdx);
+            } else if (order == 3) {
+                trains[trainIdx] <<= 1;
+                trains[trainIdx] &= ~(1 << 20);
+            } else {
+                trains[trainIdx] >>= 1;
+                trains[trainIdx] &= ~(1 << 20);
             }
         }
 
-        Set<String> set = new HashSet<>();
-        for (StringBuilder train : trains) {
-            set.add(train.toString());
+        Set<Integer> set = new HashSet<>();
+        for (int train : trains) {
+            set.add(train);
         }
         System.out.println(set.size());
     }
