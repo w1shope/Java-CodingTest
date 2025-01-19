3bff65d34f02b0284aba20c6c0db4382820b156f
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-19 14:14:26 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 104 ms, Memory: 14336 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -9,14 +10,24 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         Set<Integer> set = new HashSet<>();
-        for (int i = 0; i < 28; i++) {
-            set.add(Integer.parseInt(br.readLine()));
+        for (int i = 1; i <= 30; i++) {
+            set.add(i);
         }
 
-        for (int i = 1; i <= 30; i++) {
-            if (!set.contains(i)) {
-                System.out.println(i);
-            }
+        for (int i = 0; i < 28; i++) {
+            set.remove(Integer.parseInt(br.readLine()));
         }
+
+        int[] result = new int[2];
+        int i = 0;
+        for (int num : set) {
+            result[i++] = num;
+        }
+        Arrays.sort(result);
+
+        for (int num : result) {
+            System.out.println(num);
+        }
+
     }
 }
