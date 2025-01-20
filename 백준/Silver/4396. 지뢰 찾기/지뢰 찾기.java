a88600ce9db2ed6580af1a72aeb93a28ce766e6f
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-20 11:37:12 +0900

    [Silver IV] Title: 지뢰 찾기, Time: 104 ms, Memory: 14092 KB -BaekjoonHub

--- 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.java
+++ 백준/Silver/4396. 지뢰 찾기/지뢰 찾기.java
@@ -1,79 +1,81 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.Queue;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
-    static boolean[][] lands;
+    static int N;
+    static int[][] lands;
+    static List<int[]> bombs = new ArrayList<>();
     static int[] dx = {0, 0, -1, 1, -1, 1, -1, 1};
     static int[] dy = {-1, 1, 0, 0, -1, -1, 1, 1};
+    static char[][] result;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        int N = Integer.parseInt(br.readLine());
-
-        lands = new boolean[N][N];
-        Queue<int[]> bombQue = new ArrayDeque<>();
+        N = Integer.parseInt(br.readLine());
+        lands = new int[N][N];
         for (int i = 0; i < N; i++) {
-            char[] row = br.readLine().toCharArray();
+            char[] arr = br.readLine().toCharArray();
             for (int j = 0; j < N; j++) {
-                if (row[j] == '*') {
-                    lands[i][j] = true;
-                    bombQue.offer(new int[]{i, j});
+                if (arr[j] == '*') {
+                    bombs.add(new int[]{i, j});
                 }
+                lands[i][j] = arr[j];
             }
         }
 
-        char[][] result = new char[N][N];
+        result = new char[N][N];
         boolean isMeetBomb = false;
         for (int i = 0; i < N; i++) {
-            char[] row = br.readLine().toCharArray();
+            char[] arr = br.readLine().toCharArray();
             for (int j = 0; j < N; j++) {
-                if (row[j] == '.') {
-                    result[i][j] = '.';
-                } else {
-                    if (lands[i][j]) {
+                if (arr[j] == 'x') {
+                    result[i][j] = (char) (getAroundBombsCount(j, i) + '0');
+                    if (lands[i][j] == '*') {
                         isMeetBomb = true;
-                    } else {
-                        result[i][j] = (char) ('0' + getAroundBombCount(j, i));
                     }
+                } else {
+                    result[i][j] = '.';
                 }
             }
         }
 
         if (isMeetBomb) {
-            while (!bombQue.isEmpty()) {
-                int[] position = bombQue.poll();
-                result[position[0]][position[1]] = '*';
+            for (int i = 0; i < bombs.size(); i++) {
+                int[] pos = bombs.get(i);
+                result[pos[0]][pos[1]] = '*';
             }
         }
 
-        StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 sb.append(result[i][j]);
             }
             sb.append("\n");
         }
-
-        System.out.print(sb);
+        System.out.println(sb);
     }
 
-    static int getAroundBombCount(int x, int y) {
+    static int getAroundBombsCount(int x, int y) {
         int count = 0;
         for (int d = 0; d < 8; d++) {
             int cx = x + dx[d];
             int cy = y + dy[d];
-            if (cx < 0 || cy < 0 || cx >= lands.length || cy >= lands.length) {
+
+            if (cx < 0 || cy < 0 || cx >= N || cy >= N) {
                 continue;
             }
-            if (lands[cy][cx]) {
+
+            if (lands[cy][cx] == '*') {
                 count++;
             }
         }
+
         return count;
     }
 }
