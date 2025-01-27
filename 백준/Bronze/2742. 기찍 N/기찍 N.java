48ca7d5bdd1799077348019259322e58100a092c
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-27 20:49:33 +0900

    [Bronze IV] Title: 기찍 N, Time: 188 ms, Memory: 17888 KB -BaekjoonHub

--- 백준/Bronze/2742. 기찍 N/기찍 N.java
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -1,18 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        for (int i = n; i >= 1; i--) 
-            sb.append(i + "\n");
+        int N = Integer.parseInt(br.readLine());
+        for (int i = N; i > 0; i--) {
+            sb.append(i).append("\n");
+        }
         System.out.println(sb);
     }
 }
