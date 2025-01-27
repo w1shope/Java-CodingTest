6ca08810b0f47095a507bb7b0ab6e4f219331ec2
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-27 14:54:53 +0900

    [Gold IV] Title: 트리 순회, Time: 576 ms, Memory: 75008 KB -BaekjoonHub

--- 백준/Gold/22856. 트리 순회/트리 순회.java
+++ 백준/Gold/22856. 트리 순회/트리 순회.java
@@ -5,63 +5,63 @@ import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
 
-
     static int N;
-    static List<List<Integer>> graph = new ArrayList<>();
-    static int count = -1;
+    static List<List<Integer>> nodes = new ArrayList<>();
     static int lastNode;
     static Set<Integer> visited = new HashSet<>();
+    static int moveCount = -1;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         N = Integer.parseInt(br.readLine());
         for (int i = 0; i <= N; i++) {
-            graph.add(new ArrayList<>());
+            nodes.add(new ArrayList<>());
         }
 
         for (int i = 0; i < N; i++) {
-            String[] inputs = br.readLine().split(" ");
-            int parent = Integer.parseInt(inputs[0]);
-            int left = Integer.parseInt(inputs[1]);
-            int right = Integer.parseInt(inputs[2]);
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int v = Integer.parseInt(st.nextToken());
+            int left = Integer.parseInt(st.nextToken());
+            int right = Integer.parseInt(st.nextToken());
 
-            graph.get(parent).add(left);
-            graph.get(parent).add(right);
-            visited.add(parent);
+            nodes.get(v).add(left);
+            nodes.get(v).add(right);
         }
 
         findLastNode(1);
-        dfs(1);
+        traverse(1);
+
+        System.out.println(moveCount);
     }
 
-    static void findLastNode(int node) {
-        if (graph.get(node).get(0) != -1) {
-            findLastNode(graph.get(node).get(0));
+    static void findLastNode(int v) {
+        lastNode = v;
+        if (nodes.get(v).get(1) == -1) {
+            return;
         }
-        lastNode = node;
-        if (graph.get(node).get(1) != -1) {
-            findLastNode(graph.get(node).get(1));
-        }
+
+        findLastNode(nodes.get(v).get(1));
     }
 
-    static void dfs(int node) {
-        count++;
-        visited.remove(node);
+    static void traverse(int v) {
+        moveCount++;
+        visited.add(v);
 
-        for (int child : graph.get(node)) {
+        for (int child : nodes.get(v)) {
             if (child != -1) {
-                dfs(child);
-                count++;
+                traverse(child);
+                moveCount++;
+            }
+            if (visited.size() == N && v == lastNode) {
+                System.out.println(moveCount);
+                System.exit(0);
             }
         }
-
-        if (visited.isEmpty() && node == lastNode) {
-            System.out.println(count);
-            System.exit(0);
-        }
     }
+
 }
