f9462901a237e519e466a3784f132d0039d92b38
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-27 15:59:45 +0900

    [Gold III] Title: 폴더 정리 (small), Time: 788 ms, Memory: 89504 KB -BaekjoonHub

--- 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
+++ 백준/Gold/22860. 폴더 정리 （small）/폴더 정리 （small）.java
@@ -1,78 +1,71 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
+import java.util.StringTokenizer;
 
 public class Main {
 
+    static final String PREFIX_FOLDER = "FOLDER_";
+    static final String PREFIX_FILE = "FILE_";
+
     static int N, M;
-    static Map<String, Set<String>> map = new HashMap<>();
+    static Map<String, ArrayList<String>> map = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        String[] inputs = br.readLine().split(" ");
-        N = Integer.parseInt(inputs[0]);
-        M = Integer.parseInt(inputs[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
         for (int i = 0; i < N + M; i++) {
-            inputs = br.readLine().split(" ");
-            String parent = "folder_" + inputs[0];
-            String now = inputs[1];
-            boolean isFolder = inputs[2].equals("1");
+            st = new StringTokenizer(br.readLine());
+            String topFolder = PREFIX_FOLDER + st.nextToken();
+            String fileOrFolder = st.nextToken();
 
-            if (isFolder) {
-                now = "folder_" + now;
-            } else {
-                now = "file_" + now;
-            }
+            boolean isFolder = st.nextToken().equals("1");
+            fileOrFolder = (isFolder ? PREFIX_FOLDER : PREFIX_FILE) + fileOrFolder;
 
-            if (!map.containsKey(parent)) {
-                Set<String> set = new HashSet<>();
-                set.add(now);
-                map.put(parent, set);
+            if (map.containsKey(topFolder)) {
+                map.get(topFolder).add(fileOrFolder);
             } else {
-                Set<String> set = map.get(parent);
-                set.add(now);
+                ArrayList<String> folder = new ArrayList<>();
+                folder.add(fileOrFolder);
+                map.put(topFolder, folder);
             }
         }
 
-        StringBuilder sb = new StringBuilder();
-        int Q = Integer.parseInt(br.readLine());
-        for (int i = 0; i < Q; i++) {
+        int K = Integer.parseInt(br.readLine());
+        while (K-- > 0) {
             String[] split = br.readLine().split("/");
-            String findFolder = "folder_" + split[split.length - 1];
-
-            if (!map.containsKey(findFolder)) {
-                sb.append("0 0\n");
-                continue;
-            }
+            String folderName = PREFIX_FOLDER + split[split.length - 1];
 
             Set<String> files = new HashSet<>();
-            int fileCount = getFileCount(map.get(findFolder), files);
-            sb.append(files.size() + " " + fileCount).append("\n");
+            int fileCount = getFileCount(folderName, files);
+            sb.append(files.size()).append(" ").append(fileCount).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static int getFileCount(Set<String> set, Set<String> files) {
-        int count = 0;
+    static int getFileCount(String folderName, Set<String> fileAndFolders) {
+        if (!map.containsKey(folderName)) {
+            return 0;
+        }
 
-        Object[] objects = set.toArray();
-        for (Object name : objects) {
-            if (((String) name).startsWith("file_")) {
-                count++;
-                files.add((String) name);
+        int count = 0;
+        for (String name : map.get(folderName)) {
+            if (name.startsWith(PREFIX_FOLDER)) {
+                count += getFileCount(name, fileAndFolders);
             } else {
-                if (!map.containsKey(name)) {
-                    continue;
-                }
-                int tmp = getFileCount(map.get(name), files);
-                count += tmp;
+                count++;
+                fileAndFolders.add(name);
             }
         }
 
