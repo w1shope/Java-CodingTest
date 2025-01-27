2bcb74ed77835bd783f872e42ec58c65500a1a2f
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-27 23:46:07 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 100 ms, Memory: 14240 KB -BaekjoonHub

--- 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -6,11 +6,13 @@ public class Main {
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
         String input;
-        while ((input = br.readLine()) != null)
-            sb.append(input + "\n");
+        while ((input = br.readLine()) != null) {
+            sb.append(input).append("\n");
+        }
+
         System.out.println(sb);
     }
 }
