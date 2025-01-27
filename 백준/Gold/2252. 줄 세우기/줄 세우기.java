d2eb586aa0e5f27be1a2a92b39d01a02ea2cd5cf
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-27 13:03:11 +0900

    [Gold III] Title: 줄 세우기, Time: 412 ms, Memory: 47020 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -0,0 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    static int N, M;
+    static int[] inDegree;
+    static List<List<Integer>> graph = new ArrayList<>();
+    static Queue<Integer> que = new ArrayDeque<>();
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        inDegree = new int[N + 1];
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int v1 = Integer.parseInt(st.nextToken());
+            int v2 = Integer.parseInt(st.nextToken());
+            graph.get(v1).add(v2);
+            inDegree[v2]++;
+        }
+
+        for (int v = 1; v <= N; v++) {
+            if (inDegree[v] == 0) {
+                que.offer(v);
+            }
+        }
+
+        while (!que.isEmpty()) {
+            int v = que.poll();
+            sb.append(v).append(" ");
+
+            for (int node : graph.get(v)) {
+                inDegree[node]--;
+                if (inDegree[node] == 0) {
+                    que.offer(node);
+                }
+            }
+        }
+
+        System.out.println(sb);
+    }
+}
