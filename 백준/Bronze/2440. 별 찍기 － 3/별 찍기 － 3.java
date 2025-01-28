832a27c90a688f87500195e9b583d67117252bd7
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-28 13:15:00 +0900

    [Bronze IV] Title: 별 찍기 - 3, Time: 112 ms, Memory: 14236 KB -BaekjoonHub

--- 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
+++ 백준/Bronze/2440. 별 찍기 － 3/별 찍기 － 3.java
@@ -1,21 +1,21 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuffer sb = new StringBuffer();
+        StringBuilder sb = new StringBuilder();
 
-        int n = Integer.parseInt(br.readLine());
-        for(int i = n; i > 0; i--) {
-            for (int j = 0; j < i; j++)
+        int N = Integer.parseInt(br.readLine());
+        for (int i = N; i >= 1; i--) {
+            for (int j = 0; j < i; j++) {
                 sb.append("*");
+            }
             sb.append("\n");
         }
+
         System.out.println(sb);
     }
 }
