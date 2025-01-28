c2121fe256abe0c7efad2c35e71acd563b93fc87
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-28 21:37:41 +0900

    [Silver V] Title: 색종이, Time: 100 ms, Memory: 14204 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.java
+++ 백준/Silver/2563. 색종이/색종이.java
@@ -1,43 +1,35 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
 
-    static boolean[][] map = new boolean[100][100];
+    static int[][] arr = new int[101][101];
+    static int N;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int n = Integer.parseInt(br.readLine());
 
-        for (int i = 0; i < n; i++) {
-            String[] input = br.readLine().split(" ");
-            int x = Integer.parseInt(input[0]);
-            int y = Integer.parseInt(input[1]);
-            paint(x, y);
-        }
+        N = Integer.parseInt(br.readLine());
+        while (N-- > 0) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
 
-        System.out.println(getAreaSum());
-    }
-
-    // 도화지를 색칠하는 메서드
-    static void paint(int x, int y) {
-        for (int i = y; i < y + 10; i++) {
-            for (int j = x; j < x + 10; j++) {
-                map[i][j] = true; // true는 검은색으로 색칠된 영역임.
-            }
-        }
-    }
-
-    static int getAreaSum() {
-        int sum = 0;
-        for (int y = 0; y < 100; y++) {
-            for (int x = 0; x < 100; x++) {
-                if (map[y][x]) { // 검은색으로 칠해진 영역인 경우
-                    sum++;
+            for (int i = y; i < y + 10; i++) {
+                for (int j = x; j < x + 10; j++) {
+                    arr[i][j] = 1;
                 }
             }
         }
-        return sum;
+
+        int sum = 0;
+        for (int i = 1; i <= 100; i++) {
+            for (int j = 1; j <= 100; j++) {
+                sum += arr[i][j] == 1 ? 1 : 0;
+            }
+        }
+        System.out.println(sum);
     }
 }
