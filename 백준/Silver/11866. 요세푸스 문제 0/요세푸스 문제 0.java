5c5a8c2d4dea29defe7aa36bb0036f15138a8251
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-28 22:00:45 +0900

    [Silver IV] Title: 요세푸스 문제 0, Time: 136 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.java
@@ -3,32 +3,36 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayDeque;
 import java.util.Queue;
+import java.util.StringTokenizer;
 
 public class Main {
+
+    static Queue<Integer> que = new ArrayDeque<>();
+    static int N, M;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        String[] inputs = br.readLine().split(" ");
-        int N = Integer.parseInt(inputs[0]);
-        int K = Integer.parseInt(inputs[1]);
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
 
-        Queue<Integer> que = new ArrayDeque<>();
         for (int i = 1; i <= N; i++) {
             que.offer(i);
         }
 
+        int count = 1;
         StringBuilder sb = new StringBuilder();
-
         sb.append("<");
         while (que.size() > 1) {
-            for (int i = 0; i < K - 1; i++) {
+            if (count % M == 0) {
+                sb.append(que.poll()).append(", ");
+            } else {
                 que.offer(que.poll());
             }
-
-            sb.append(que.poll()).append(", ");
+            count++;
         }
-        sb.append(que.poll());
-        sb.append(">");
+        sb.append(que.poll()).append(">");
 
         System.out.println(sb);
     }
