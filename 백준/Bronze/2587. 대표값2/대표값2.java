9bab5de14ded44ed362753c6a246d2939dc43b9a
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-29 14:13:20 +0900

    [Bronze II] Title: 대표값2, Time: 104 ms, Memory: 14080 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,19 +1,36 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int[] arr = new int[5];
-        for (int i = 0; i < 5; i++)
-            arr[i] = Integer.parseInt(br.readLine());
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-        Arrays.sort(arr);
+        int sum = 0;
+        for (int i = 0; i < 5; i++) {
+            int val = Integer.parseInt(br.readLine());
+            sum += val;
 
-        System.out.println(Arrays.stream(arr).sum() / 5);
-        System.out.println(arr[2]);
+            if (minHeap.size() == maxHeap.size()) {
+                maxHeap.add(val);
+            } else {
+                minHeap.add(val);
+            }
+
+            if (!minHeap.isEmpty()) {
+                if (maxHeap.peek() > minHeap.peek()) {
+                    minHeap.add(maxHeap.poll());
+                    maxHeap.add(minHeap.poll());
+                }
+            }
+        }
+
+        System.out.println(sum / 5);
+        System.out.println(maxHeap.poll());
     }
 }
