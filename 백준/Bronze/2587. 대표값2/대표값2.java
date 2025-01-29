95fda648f1ff9b5d6fc463dd9f783769081b4e35
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-29 14:14:15 +0900

    [Bronze II] Title: 대표값2, Time: 100 ms, Memory: 14164 KB -BaekjoonHub

--- 백준/Bronze/2587. 대표값2/대표값2.java
+++ 백준/Bronze/2587. 대표값2/대표값2.java
@@ -1,36 +1,24 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
-
         int sum = 0;
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < 5; i++) {
             int val = Integer.parseInt(br.readLine());
             sum += val;
-
-            if (minHeap.size() == maxHeap.size()) {
-                maxHeap.add(val);
-            } else {
-                minHeap.add(val);
-            }
-
-            if (!minHeap.isEmpty()) {
-                if (maxHeap.peek() > minHeap.peek()) {
-                    minHeap.add(maxHeap.poll());
-                    maxHeap.add(minHeap.poll());
-                }
-            }
+            list.add(val);
         }
+        Collections.sort(list);
 
         System.out.println(sum / 5);
-        System.out.println(maxHeap.poll());
+        System.out.println(list.get(2));
     }
 }
