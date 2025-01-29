29fdfc1fde081539d14ba8da0471081b8cf97238
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-29 12:52:36 +0900

    [Silver III] Title: 통계학, Time: 632 ms, Memory: 71680 KB -BaekjoonHub

--- 백준/Silver/2108. 통계학/통계학.java
+++ 백준/Silver/2108. 통계학/통계학.java
@@ -1,67 +1,65 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
 import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.PriorityQueue;
 
 public class Main {
-
-    static int N;
-    static List<Integer> list = new ArrayList<>();
-    static Map<Integer, Integer> map = new HashMap<>();
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        N = Integer.parseInt(br.readLine());
+        int[] frequency = new int[8001];
+        int min = 4000, max = -4000;
+        int sum = 0;
 
-        if (N == 1) {
-            int val = Integer.parseInt(br.readLine());
-            System.out.println(val);
-            System.out.println(val);
-            System.out.println(val);
-            System.out.println(0);
-            return;
-        }
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
 
-        int sum = 0; // 1. 산술평균
+        int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             int val = Integer.parseInt(br.readLine());
             sum += val;
-            map.put(val, map.getOrDefault(val, 0) + 1);
-            list.add(val);
-        }
-        Collections.sort(list);
+            frequency[val + 4000]++;
 
-        int mid = list.get((N - 1) / 2); // 2. 중앙값
-        int range = list.get(N - 1) - list.get(0); // 4. 범위
+            min = Math.min(min, val);
+            max = Math.max(max, val);
 
-        PriorityQueue<int[]> que = new PriorityQueue<>((a, b) -> {
-            if (a[1] == b[1]) {
-                return a[0] - b[0];
+            if (minHeap.size() == maxHeap.size()) {
+                maxHeap.offer(val);
+            } else {
+                minHeap.offer(val);
             }
-            return b[1] - a[1];
-        });
-        for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            que.offer(new int[]{entry.getKey(), entry.getValue()});
+
+            if (!minHeap.isEmpty()) {
+                if (maxHeap.peek() > minHeap.peek()) {
+                    minHeap.offer(maxHeap.poll());
+                    maxHeap.offer(minHeap.poll());
+                }
+            }
         }
 
-        int mode = -1;
-        int[] poll1 = que.poll();
-        int[] poll2 = que.poll();
-        if (poll1[1] == poll2[1]) {
-            mode = poll2[0];
-        } else {
-            mode = poll1[0];
+        double avg = Math.round((double) sum / N);
+        int mid = maxHeap.poll();
+        int range = max - min;
+
+        int mostFrequency = 0;
+        boolean isDuplicate = false;
+
+        for (int i = 1; i < 8001; i++) {
+            if (frequency[mostFrequency] == frequency[i]) {
+                if (!isDuplicate) {
+                    isDuplicate = true;
+                    mostFrequency = i;
+                }
+            } else if (frequency[mostFrequency] < frequency[i]) {
+                isDuplicate = false;
+                mostFrequency = i;
+            }
         }
 
-        System.out.println((int) Math.round((double) sum / N));
+        System.out.println((int) avg);
         System.out.println(mid);
-        System.out.println(mode);
+        System.out.println(mostFrequency - 4000);
         System.out.println(range);
     }
 }
