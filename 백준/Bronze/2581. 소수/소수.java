0199aaee2ca67f196272ab8cce56df1cf8176d8e
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-30 14:38:14 +0900

    [Bronze II] Title: 소수, Time: 108 ms, Memory: 14044 KB -BaekjoonHub

--- 백준/Bronze/2581. 소수/소수.java
+++ 백준/Bronze/2581. 소수/소수.java
@@ -12,10 +12,6 @@ public class Main {
         int min = -1;
         int sum = 0;
         for (int i = N; i <= M; i++) {
-            if (i == 1) {
-                continue;
-            }
-
             if (isPrimeNumber(i)) {
                 sum += i;
                 min = min == -1 ? i : min;
@@ -31,11 +27,22 @@ public class Main {
     }
 
     static boolean isPrimeNumber(int num) {
-        for (int i = 2; i < num; i++) {
+        if (num == 1) {
+            return false;
+        }
+        if (num == 2) {
+            return true;
+        }
+        if (num % 2 == 0) {
+            return false;
+        }
+
+        for (int i = 3; i * i <= num; i++) {
             if (num % i == 0) {
                 return false;
             }
         }
+
         return true;
     }
 }
