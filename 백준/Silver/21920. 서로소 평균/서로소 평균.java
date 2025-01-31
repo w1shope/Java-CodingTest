abb2cf83fc0c265113e1940614725487376ac183
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-31 11:12:26 +0900

    [Silver IV] Title: 서로소 평균, Time: 428 ms, Memory: 54204 KB -BaekjoonHub

--- 백준/Silver/21920. 서로소 평균/서로소 평균.java
+++ 백준/Silver/21920. 서로소 평균/서로소 평균.java
@@ -1,31 +1,31 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = Integer.parseInt(br.readLine());
-        int[] nums = new int[n];
-        String[] inputs = br.readLine().split(" ");
-        for (int i = 0; i < n; i++) {
-            nums[i] = Integer.parseInt(inputs[i]);
+        int N = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
 
-        int x = Integer.parseInt(br.readLine());
-
+        long sum = 0L;
         int count = 0;
-        double sum = 0d;
-        for (int num : nums) {
-            int result = gcd(num, x);
-            if (result == 1) {
+        int X = Integer.parseInt(br.readLine());
+        for (int num : arr) {
+            if (gcd(num, X) == 1) {
                 sum += num;
                 count++;
             }
         }
 
-        System.out.println(sum / count);
+        System.out.println((double) sum / count);
     }
 
     static int gcd(int a, int b) {
