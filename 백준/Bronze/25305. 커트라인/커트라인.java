b9510189b232d666f4cb9793196e20f73f44a2d7
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-02-01 02:43:33 +0900

    [Bronze II] Title: 커트라인, Time: 120 ms, Memory: 14520 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -1,18 +1,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int n = Integer.parseInt(input[0]);
-        int k = Integer.parseInt(input[1]);
-        int[] arr = new int[n];
-        input = br.readLine().split(" ");
-        for (int i = 0; i < n; i++)
-            arr[i] = Integer.parseInt(input[i]);
-        System.out.println(Arrays.stream(arr).sorted().skip(n - k).min().getAsInt());
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        st = new StringTokenizer(br.readLine());
+        while (st.hasMoreTokens()) {
+            maxHeap.add(Integer.parseInt(st.nextToken()));
+        }
+
+        int prev = -1;
+        for (int i = 0; i < K; i++) {
+            prev = maxHeap.poll();
+        }
+
+        System.out.println(prev);
     }
 }
