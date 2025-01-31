e738de421a177c48d742bfcb620e1d14f4ad53f0
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-02-01 02:44:31 +0900

    [Bronze II] Title: 커트라인, Time: 116 ms, Memory: 14748 KB -BaekjoonHub

--- 백준/Bronze/25305. 커트라인/커트라인.java
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -1,8 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Collections;
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -13,17 +12,13 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Integer> maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        int[] arr = new int[N];
         st = new StringTokenizer(br.readLine());
-        while (st.hasMoreTokens()) {
-            maxHeap.add(Integer.parseInt(st.nextToken()));
+        for (int i = 0; i < N; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
         }
+        Arrays.sort(arr);
 
-        int prev = -1;
-        for (int i = 0; i < K; i++) {
-            prev = maxHeap.poll();
-        }
-
-        System.out.println(prev);
+        System.out.println(arr[N - K]);
     }
 }
