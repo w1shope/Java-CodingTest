c310feae739c4063f4bc216e7c00110213d9fbf4
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-31 10:22:21 +0900

    [Bronze I] Title: 소인수분해, Time: 136 ms, Memory: 14380 KB -BaekjoonHub

--- 백준/Bronze/11653. 소인수분해/소인수분해.java
+++ 백준/Bronze/11653. 소인수분해/소인수분해.java
@@ -3,24 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+
+    static int N;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        N = Integer.parseInt(br.readLine());
+
         StringBuilder sb = new StringBuilder();
-
-        int n = Integer.parseInt(br.readLine());
-
-        for (int div = 2; ; ) {
-            if (n == 1) {
-                break;
-            }
-            if (n % div == 0) {
-                n /= div;
-                sb.append(div + "\n");
+        int div = 2;
+        while (N != 1) {
+            if (N % div == 0) {
+                sb.append(div).append("\n");
+                N /= div;
             } else {
                 div++;
             }
         }
-
         System.out.println(sb);
     }
 }
