1437c42b1f1536a08adaec461e18ae6658f05d10
Heemang Lee (이희망) <wishhopelee@gmail.com>
2025-01-31 11:50:45 +0900

    [Silver IV] Title: 에라토스테네스의 체, Time: 104 ms, Memory: 14248 KB -BaekjoonHub

--- 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
+++ 백준/Silver/2960. 에라토스테네스의 체/에라토스테네스의 체.java
@@ -1,9 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Iterator;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -15,54 +12,20 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int K = Integer.parseInt(st.nextToken());
 
+        boolean[] isDelete = new boolean[N + 1];
         int deleteCount = 0;
-        List<Integer> list = new ArrayList<>();
         for (int i = 2; i <= N; i++) {
-            list.add(i);
-        }
-
-        while (true) {
-            int primeNumber = -1;
-            for (int i = 0; i < list.size(); i++) {
-                if (isPrimeNumber(list.get(i))) {
-                    primeNumber = list.get(i);
-                    break;
-                }
-            }
-
-            Iterator<Integer> iter = list.iterator();
-            while (iter.hasNext()) {
-                int num = iter.next();
-                if (num % primeNumber == 0) {
-                    iter.remove();
+            for (int j = i; j <= N; j += i) {
+                if (!isDelete[j]) {
+                    isDelete[j] = true;
                     deleteCount++;
                 }
 
                 if (deleteCount == K) {
-                    System.out.println(num);
+                    System.out.println(j);
                     return;
                 }
             }
         }
     }
-
-    static boolean isPrimeNumber(int num) {
-        if (num == 1) {
-            return false;
-        }
-        if (num == 2) {
-            return true;
-        }
-        if (num % 2 == 0) {
-            return false;
-        }
-
-        for (int i = 3; i * i <= num; i += 2) {
-            if (num % i == 0) {
-                return false;
-            }
-        }
-        return true;
-    }
-
 }
